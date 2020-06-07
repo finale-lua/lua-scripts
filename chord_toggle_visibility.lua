ca36b0bad6612a5c890b9634c40156ee5e93f940
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-06-07 09:33:22 -0700

    Expression Baseline and Chord Visibility

---
+++ chord_toggle_visibility.lua
@@ -0,0 +1,20 @@
+function plugindef()
+    finaleplugin.RequireSelection = true
+    finaleplugin.Author = "Nick Mazuk"
+    finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
+    finaleplugin.Version = "1.0"
+    finaleplugin.Date = "July 2, 2019"
+    finaleplugin.CategoryTags = "Chord"
+    finaleplugin.AuthorURL = "https://nickmazuk.com"
+    return "Toggle Chord Visibility", "Toggle Chord Visibility", "Toggles the chords' visibility"
+end
+
+local musicRegion = finenv.Region()
+musicRegion:SetCurrentSelection()
+local chords = finale.FCChords()
+chords:LoadAllForRegion(musicRegion)
+for chord in each(chords) do
+    chord.ChordVisible = not chord:GetChordVisible()
+    chord:Save()
+end
+
