5b2545515916a9d71ce1e0728a4a0fb9cb38c1b0
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-06-19 14:48:07 -0700

    Fixed Double Octave Up/Down errors

--- pitch_entry_double_octave_down.lua
+++ pitch_entry_double_octave_down.lua
@@ -11,22 +11,16 @@ end
 
 local path = finale.FCString()
 path:SetRunningLuaFolderPath()
-package.path = package.path .. ";" .. path.LuaString .. "/library/?.lua"
-local transposition = require("transposition")
+package.path = package.path .. ";" .. path.LuaString .. "?.lua"
+local transposition = require("library.transposition")
 
 function pitch_entry_double_octave_down()
     for entry in eachentrysaved(finenv.Region()) do
         if (entry.Count == 1) then
             local note = entry:CalcLowestNote(nil)
-            local pitch_string = finale.FCString()
-            local measure = entry:GetMeasure()
-            measure_object = finale.FCMeasure()
-            measure_object:Load(measure)
-            local keysig = measure_object:GetKeySignature()
-            note:GetString(pitch_string, keysig, false, false)
-            pitch_string = transposition.change_octave(pitch_string, -1)
             local new_note = entry:AddNewNote()
-            new_note:SetString(pitch_string, keysig, false)
+            transposition.set_notes_to_same_pitch(note, new_note)
+            transposition.change_octave(new_note, -1)
             new_note.Tie = note.Tie
             new_note.TieBackwards = note.TieBackwards
         end
