3761a602d98614bfa9fdd1835f9ac640bae0eef8
Robert Patterson <robert@robertgpatterson.com>
2020-06-20 08:05:32 -0500

    provisional checkin

--- measure_numbers_adjust_for_leadin.lua
+++ measure_numbers_adjust_for_leadin.lua
@@ -21,11 +21,38 @@ local barline_thickness = math.floor(size_prefs.ThinBarlineThickness/64.0 + 0.5)
 
 local additional_offset = 0 -- add more here evpu to taste (positive values move the number to the right)
 
+local is_mid_system_default_number_visible = function (meas_num_region, cell, system, current_is_part)
+    if cell.Measure == system.FirstMeasure then
+        return false
+    end
+    if not meas_num_region:GetShowMultiples(current_is_part) then
+        return false
+    end
+    local staff = finale.FCCurrentStaffSpec()
+    if not staff:LoadForCell(cell, 0) then
+        return false
+    end
+    if staff.ShowMeasureNumbers then
+        return true
+    end
+    if meas_num_region:GetShowOnTopStaff() and (cell.Staff == system.TopStaff) then
+        return true
+    end
+    if meas_num_region:GetShowOnBottomStaff() and (cell.Staff == system:CalcBottomStaff()) then
+        return true
+    end
+    return false
+end
+
 function measure_numbers_adjust_for_leadin()
     local systems = finale.FCStaffSystems()
     systems:LoadAll()
     local meas_num_regions = finale.FCMeasureNumberRegions()
     meas_num_regions:LoadAll()
+    local parts = finale.FCParts()
+    parts:LoadAll()
+    local current_part = parts:GetCurrent()
+    local current_is_part = not current_part:IsScore()
 
     for meas_num, staff in eachcell(finenv.Region()) do
         local system = systems:FindMeasureNumber(meas_num)
@@ -35,38 +62,40 @@ function measure_numbers_adjust_for_leadin()
                 local cell_metrics = finale.FCCellMetrics()
                 local cell = finale.FCCell(meas_num, staff)
                 if cell_metrics:LoadAtCell(cell) and (cell_metrics.StaffScaling ~= 0) then --metrics are all zero for measures inside mm rests, so skip those
-                    -- a refinement would be to determine if measure numbers are showing on this staff and skip the cell if not
-                    local lead_in = cell_metrics.MusicStartPos - cell_metrics:GetLeftEdge()
-                    -- FCCellMetrics currently does not provide the barline width, which is available in the underlying PDK struct.
-                    -- if it did, we would subtract it here. Instead use the hard-coded value above
-                    lead_in = lead_in - barline_thickness
-                    if (0 ~= lead_in) then
-                        lead_in = lead_in - additional_offset
-                    end
-                    -- Finale scales the lead_in by the staff percent, so remove that if any
-                    local staff_percent = (cell_metrics.StaffScaling / 10000.0) / (cell_metrics.SystemScaling / 10000.0)
-                    lead_in = math.floor(lead_in/staff_percent + 0.5)
-                    -- FCSeparateMeasureNumber is scaled horizontally by the horizontal stretch, so back that out
-                    local horz_percent = cell_metrics.HorizontalStretch / 10000.0
-                    lead_in = math.floor(lead_in/horz_percent + 0.5)
-                    local sep_nums = finale.FCSeparateMeasureNumbers()
-                    sep_nums:LoadAllInCell(cell)
-                    if (sep_nums.Count > 0) then
-                        for sep_num in each(sep_nums) do
-                            sep_num.HorizontalPosition = -lead_in
-                            sep_num:Save()
+                    if is_mid_system_default_number_visible(meas_num_region, cell, system, current_is_part) then
+                        local lead_in = cell_metrics.MusicStartPos - cell_metrics:GetLeftEdge()
+                        -- FCCellMetrics currently does not provide the barline width, which is available in the underlying PDK struct.
+                        -- if it did, we would subtract it here. Instead use the valus derived from document settings above.
+                        lead_in = lead_in - barline_thickness
+                        if (0 ~= lead_in) then
+                            print ("lead in found at cell ", cell.Measure, " ", cell.Staff)
+                            lead_in = lead_in - additional_offset
+                            -- Finale scales the lead_in by the staff percent, so remove that if any
+                            local staff_percent = (cell_metrics.StaffScaling / 10000.0) / (cell_metrics.SystemScaling / 10000.0)
+                            lead_in = math.floor(lead_in/staff_percent + 0.5)
+                            -- FCSeparateMeasureNumber is scaled horizontally by the horizontal stretch, so back that out
+                            local horz_percent = cell_metrics.HorizontalStretch / 10000.0
+                            lead_in = math.floor(lead_in/horz_percent + 0.5)
                         end
-                    elseif (0 ~= lead_in) then
-                        local sep_num = finale.FCSeparateMeasureNumber()
-                        sep_num:ConnectCell(cell)
-                        sep_num:AssignMeasureNumberRegion(meas_num_region)
-                        sep_num.HorizontalPosition = -lead_in
-                        --sep_num:SetShowOverride(true) -- enable this line if you want to force show the number. It will be force shown on every selected staff
-                        if sep_num:SaveNew() then
-                            local measure = finale.FCMeasure()
-                            measure:Load(meas_num)
-                            measure:SetContainsManualMeasureNumbers(true)
-                            measure:Save()
+                        local sep_nums = finale.FCSeparateMeasureNumbers()
+                        sep_nums:LoadAllInCell(cell)
+                        if (sep_nums.Count > 0) then
+                            for sep_num in each(sep_nums) do
+                                sep_num.HorizontalPosition = -lead_in
+                                sep_num:Save()
+                            end
+                        elseif (0 ~= lead_in) then
+                            local sep_num = finale.FCSeparateMeasureNumber()
+                            sep_num:ConnectCell(cell)
+                            sep_num:AssignMeasureNumberRegion(meas_num_region)
+                            sep_num.HorizontalPosition = -lead_in
+                            --sep_num:SetShowOverride(true) -- enable this line if you want to force show the number. otherwise it will show or hide based on the measure number region
+                            if sep_num:SaveNew() then
+                                local measure = finale.FCMeasure()
+                                measure:Load(meas_num)
+                                measure:SetContainsManualMeasureNumbers(true)
+                                measure:Save()
+                            end
                         end
                     end
                 end
