44e41ad3dd4f45b31189d729ab1616b779f10249
Robert Patterson <robert@robertgpatterson.com>
2020-06-27 15:20:37 -0500

    working version of expression find orphans

--- expression_find_orphaned_definitions.lua
+++ expression_find_orphaned_definitions.lua
@@ -22,9 +22,9 @@ local new_line_string = "\n"
 local max_value_to_search = 32767
 
 local get_report_string_for_orphans = function(orphaned_exps, is_for_shape)
-    local type_string = "   Text Expression"
+    local type_string = "Text Expression"
     if is_for_shape then
-        type_string = "   Shape Expression"
+        type_string = "Shape Expression"
     end
     local report_string = ""
     local is_first = true
@@ -81,33 +81,6 @@ local expression_find_orphans_for_type = function(is_for_shape)
     return orphaned_exps, max_valid, max_found
 end
 
-local expression_insert_dummy_exps_for_type = function(max_valid, max_found, is_for_shape)
-    for item_no = max_valid+1, max_found do
-        if is_for_shape then
-            exp_def = finale.FCShapeExpressionDef()
-        else
-            exp_def = finale.FCTextExpressionDef()
-        end
-        if not exp_def:Load(item_no) then
-            local desc = exp_def:CreateDescription()
-            desc.LuaString = "Dummy expression added to reconnect orphaned expression"
-            --exp_def:SetDescription(desc)
-            if is_for_shape then
-                -- not sure what to do here; try leaving it with no shape
-            else
-                local text = finale.FCString()
-                text.LuaString = "Dummy expression"
-                exp_def:SaveNewTextBlock(text)
-            end
-            if not exp_def:DeepSaveAs(item_no) then
-                finenv.UI():AlertInfo("SaveAs " .. item_no .. " failed", "DEBUG")
-                return false
-            end
-        end
-    end
-    return true
-end
-
 function expression_find_orphaned_definitions()
     local orphaned_text_exps, text_max_valid, text_max_found = expression_find_orphans_for_type(false)
     local orphaned_shape_exps, shape_max_valid, shape_max_found = expression_find_orphans_for_type(true)
@@ -126,27 +99,8 @@ function expression_find_orphaned_definitions()
         report_string = report_string .. get_report_string_for_orphans(orphaned_shape_exps, true)
     end
     if got_orphan then
-        report_string = report_string .. new_line_string .. new_line_string .. "Max Valid Text = " .. text_max_valid .. " Max Valid Shape = " .. shape_max_valid
+        report_string = report_string .. new_line_string .. new_line_string .. "Max Valid Text = " .. text_max_valid .. ". Max Valid Shape = " .. shape_max_valid .. "."
         finenv.UI():AlertInfo(report_string, "Found Orphaned Expressions:")
-        if finale.YESRETURN == finenv.UI():AlertYesNo("Dummy expressions will be inserted into the gaps so that the Expression Selection Dialog sees all the orphaned expressions.", "Reconnect Them?") then
-            finenv.StartNewUndoBlock("Reconnect Orphaned Expressions", false)
-            if #orphaned_text_exps > 0 then
-                local text_result = expression_insert_dummy_exps_for_type(text_max_valid, text_max_found, false)
-                if not text_result then
-                    finenv.UI():AlertError("Unable To Reconnect Orphaned Text Expressions", "Text Expressions")
-                    finenv.StartNewUndoBlock("Reconnect Orphaned Expressions", false)
-                    return
-                end
-            end
-            if #orphaned_shape_exps > 0 then
-                local shape_result = expression_insert_dummy_exps_for_type(shape_max_valid, shape_max_found, true)
-                if not shape_result then
-                    finenv.UI():AlertError("Unable To Reconnect Orphaned Shape Expressions", "Shape Expressions")
-                    finenv.StartNewUndoBlock("Reconnect Orphaned Expressions", false)
-                    return
-                end
-            end
-        end
     else
         finenv.UI():AlertInfo("", "No Orphaned Expressions Found")
     end
