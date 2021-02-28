38b6cdce91ca9899642b15c02059509cd7bd3870
Robert Patterson <robert@robertgpatterson.com>
2021-02-28 13:06:21 -0600

    updated version resets single-staff exps only

--- expression_reset_positioning.lua
+++ expression_reset_positioning.lua
@@ -4,7 +4,7 @@ function plugindef()
     finaleplugin.Version = "1.0"
     finaleplugin.Date = "February 28, 2020"
     finaleplugin.CategoryTags = "Expression"
-    return "Reset Expression Positions", "Reset Expression Positions", "Resets the assignment position of all selected expressions."
+    return "Reset Expression Positions", "Reset Expression Positions", "Resets the assignment position of all selected single-staff expressions."
 end
 
 function expression_reset_positioning()
@@ -14,16 +14,18 @@ function expression_reset_positioning()
     local expressions = finale.FCExpressions()
     expressions:LoadAllForRegion(finenv.Region())
     for expression in each(expressions) do
-        local do_reset = false
-        if current_part:IsScore() and expression.ScoreAssignment then
-            do_reset = true
-        end
-        if current_part:IsPart() and expression.PartAssignment then
-            do_reset = true
-        end
-        if do_reset then
-            expression:ResetPos()
-            expression:Save()
+        if 0 == expression.StaffListID then -- note: IsSingleStaffAssigned() appears to be not 100% accurate for exps with staff lists
+            local do_reset = false
+            if current_part:IsScore() and expression.ScoreAssignment then
+                do_reset = true
+            end
+            if current_part:IsPart() and expression.PartAssignment then
+                do_reset = true
+            end
+            if do_reset then
+                expression:ResetPos()
+                expression:Save()
+            end
         end
     end
 end
