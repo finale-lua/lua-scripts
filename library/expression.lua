f4f9dae12a574c4ab8385805ea2e42f51c2cad0a
Robert Patterson <robert@robertgpatterson.com>
2021-03-01 11:48:41 -0600

    shell library for exps

--- library/expression.lua
+++ library/expression.lua
@@ -2,6 +2,9 @@
 -- Simply import this file to another Lua script to use any of these scripts
 local expression = {}
 
+function expression.get_associated_entry(expression)
+end
+
 function expression.handle_offset(expression)
 end
 
