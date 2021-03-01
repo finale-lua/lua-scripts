49044cb8933dac9174f91c98372a6e5197783311
Robert Patterson <robert@robertgpatterson.com>
2021-03-01 11:44:33 -0600

    add expression library

---
+++ library/expression.lua
@@ -0,0 +1,8 @@
+-- A collection of helpful JW Lua articulation scripts
+-- Simply import this file to another Lua script to use any of these scripts
+local expression = {}
+
+function expression.handle_offset(expression)
+end
+
+return expression
