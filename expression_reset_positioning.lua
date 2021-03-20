49d554320f2c0e778fce616ed61a2dffbfbda8ec
Robert Patterson <robert@robertgpatterson.com>
2021-03-20 10:10:19 -0500

    Revised expression assignment scripts to recognize score-only/part-only assignment. Added scripts for score-only and part-only assignment.

--- expression_reset_positioning.lua
+++ expression_reset_positioning.lua
@@ -1,30 +1,27 @@
 function plugindef()
     finaleplugin.Author = "Robert Patterson"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
-    finaleplugin.Version = "1.0"
-    finaleplugin.Date = "February 28, 2020"
+    finaleplugin.Version = "1.1"
+    finaleplugin.Date = "March 20, 2021"
     finaleplugin.CategoryTags = "Expression"
     return "Reset Expression Positions", "Reset Expression Positions", "Resets the assignment position of all selected single-staff expressions."
 end
 
+local path = finale.FCString()
+path:SetRunningLuaFolderPath()
+package.path = package.path .. ";" .. path.LuaString .. "?.lua"
+local library = require("library.general_library")
+local expression = require("library.expression")
+
 function expression_reset_positioning()
-    local parts = finale.FCParts()
-    parts:LoadAll()
-    local current_part = parts:GetCurrent()
+    local current_part = library.get_current_part()
     local expressions = finale.FCExpressions()
     expressions:LoadAllForRegion(finenv.Region())
-    for expression in each(expressions) do
-        if 0 == expression.StaffListID then -- note: IsSingleStaffAssigned() appears to be not 100% accurate for exps with staff lists
-            local do_reset = false
-            if current_part:IsScore() and expression.ScoreAssignment then
-                do_reset = true
-            end
-            if current_part:IsPart() and expression.PartAssignment then
-                do_reset = true
-            end
-            if do_reset then
-                expression:ResetPos()
-                expression:Save()
+    for exp_assign in each(expressions) do
+        if 0 == exp_assign.StaffListID then -- note: IsSingleStaffAssigned() appears to be not 100% accurate for exps with staff lists
+            if expression.is_for_current_part(exp_assign, current_part) then
+                exp_assign:ResetPos()
+                exp_assign:Save()
             end
         end
     end
