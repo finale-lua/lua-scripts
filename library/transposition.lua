14b827502c7a3ea16a5dc6c1f65bfca538970d15
Robert Patterson <robert@robertgpatterson.com>
2021-03-24 08:39:38 -0500

    initial pass

--- library/transposition.lua
+++ library/transposition.lua
@@ -40,60 +40,29 @@ function transposition.pitch_string_change_octave(pitch_string, n)
 end
 
 function transposition.change_octave(note, n)
-    local pitch_string = transposition.calc_pitch_string(note)
-    pitch_string = transposition.pitch_string_change_octave(pitch_string, n)
-    transposition.set_pitch_string(note, pitch_string)
+    note.Displacement = note.Displacement + 7*n
 end
 
 function transposition.set_notes_to_same_pitch(note_a, note_b)
-    local pitch_string = transposition.calc_pitch_string(note_a)
-    transposition.set_pitch_string(note_b, pitch_string)
+    note_b.Displacement = note_a.Displacement
+    note_b.RaiseLower = note_a.RaiseLower
 end
+
 
 -- 
 -- DIATONIC transposition
 -- 
 
 function transposition.diatonic_third_down(note)
-    local pitch_string = transposition.calc_pitch_string(note)
-    local letters = "ABCDEFGABCDEFG"
-    local note_name_position = letters:find(pitch_string.LuaString:sub(1, 1))
-    local new_note = letters:sub(note_name_position + 5, note_name_position + 5)
-    pitch_string.LuaString = new_note .. pitch_string.LuaString:sub(2)
-
-    -- transposes everything an octave higher if necessary
-    if (note_name_position < 5) and note_name_position > 2 then
-        pitch_string = transposition.pitch_string_change_octave(pitch_string, -1)
-    end
-    transposition.set_pitch_string(note, pitch_string)
+    note.Displacement = note.Displacement - 2
 end
 
 function transposition.diatonic_fourth_up(note)
-    local pitch_string = transposition.calc_pitch_string(note)
-    local letters = "ABCDEFGABCDEFG"
-    local note_name_position = letters:find(pitch_string.LuaString:sub(1, 1))
-    local new_note = letters:sub(note_name_position + 3, note_name_position + 3)
-    pitch_string.LuaString = new_note .. pitch_string.LuaString:sub(2)
-
-    -- transposes everything an octave higher if necessary
-    if (note_name_position >= 7) or note_name_position <= 2 then
-        pitch_string = transposition.pitch_string_change_octave(pitch_string, 1)
-    end
-    transposition.set_pitch_string(note, pitch_string)
+    note.Displacement = note.Displacement + 3
 end
 
 function transposition.diatonic_fifth_down(note)
-    local pitch_string = transposition.calc_pitch_string(note)
-    local letters = "ABCDEFGABCDEFG"
-    local note_name_position = letters:find(pitch_string.LuaString:sub(1, 1))
-    local new_note = letters:sub(note_name_position + 3, note_name_position + 3)
-    pitch_string.LuaString = new_note .. pitch_string.LuaString:sub(2)
-
-    -- transposes everything an octave higher if necessary
-    if (note_name_position < 7) and note_name_position > 2 then
-        pitch_string = transposition.pitch_string_change_octave(pitch_string, -1)
-    end
-    transposition.set_pitch_string(note, pitch_string)
+    note.Displacement = note.Displacement + 4
 end
 
 -- 
