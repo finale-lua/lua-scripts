af1583060a9a281fef7b3901662432c91564de7a
Robert Patterson <robert@robertgpatterson.com>
2021-03-30 17:04:23 -0500

    fix issues with auto-harmonics

--- library/notehead.lua
+++ library/notehead.lua
@@ -2,21 +2,47 @@
 -- Simply import this file to another Lua script to use any of these scripts
 local notehead = {}
 
+local config = {
+    diamond_open                = 79,
+    diamond_closed              = 79,   -- per Elaine Gould, use open diamond even on closed regular notes, but allow it to be overridden
+    diamond_resize              = 110,
+    diamond_whole_offset        = 5,
+    diamond_breve_offset        = 14
+}
+
+-- use this func because CalcStemUp is not reliable for wholes or breves
+-- also it's not reliable after transposition
+local calc_stem_up = function(note)
+    if note.Entry.FreezeStem then
+        return note.Entry:CalcStemUp()
+    end
+    return (note:CalcStaffPosition() < -5) -- this works a lot of the time, and when it doesn't the user can use free-stem to make it right
+end
+
 function notehead.change_shape(note, shape)
     local notehead = finale.FCNoteheadMod()
     notehead:EraseAt(newnote)
 
     if shape == "diamond" then
-        notehead.CustomChar = 79
-        notehead.Resize = 110
         local entry = note:GetEntry()
-        if (entry:GetDuration() == 4096) then
-            if (note:CalcStaffPosition() >= -5) then
-                notehead.HorizontalPos = 5
+        local offset = 0
+        local notehead_char = config.diamond_open
+        if entry.Duration >= finale.BREVE then
+            offset = config.diamond_breve_offset
+        elseif entry.Duration >= finale.WHOLE_NOTE then
+            offset = config.diamond_whole_offset
+        elseif entry.Duration < finale.HALF_NOTE then
+            notehead_char = config.diamond_closed
+        end
+        if (0 ~= offset) then
+            if calc_stem_up(note) then
+                notehead.HorizontalPos = -1*offset
             else
-                notehead.HorizontalPos = -5
+                notehead.HorizontalPos = offset
             end
         end
+        notehead.CustomChar = notehead_char
+        notehead.Resize = config.diamond_resize
     end
 
     notehead:SaveAt(note)
