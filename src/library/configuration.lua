89f6bceb4979fda56cc06f9f1a53780e5fa7f6d5
Robert Patterson <robert@robertgpatterson.com>
2021-04-21 08:13:19 -0500

    blech misspelled

--- src/library/configuration.lua
+++ src/library/configuration.lua
@@ -23,7 +23,7 @@ Currently the following are not supported:
 - Tables embedded within tables
 - Tables containing strings that contain commas
 
-A sample configurarion file might be:
+A sample configuration file might be:
 
 ```lua
 -- Configuration File for "Hairpin and Dynamic Adjustments" script
