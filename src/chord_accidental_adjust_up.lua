553f54c1f1b1fddf9edfe2f456980119c102614b
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-08-17 15:44:41 -0700

    add configuration

--- src/chord_accidental_adjust_up.lua
+++ src/chord_accidental_adjust_up.lua
@@ -9,21 +9,29 @@ function plugindef()
     return "Chord Accidental - Move Down", "Adjust Chord Accidental Down", "Adjust the accidental of chord symbol down"
 end
 
+local path = finale.FCString()
+path:SetRunningLuaFolderPath()
+package.path = package.path .. ";" .. path.LuaString .. "?.lua"
+
+local configuration = require("library.configuration")
+local config = {vertical_increment = 5}
+
+configuration.get_parameters("note_automatic_jete.config.txt", config)
+
 function chord_accidental_adjust_up()
     local chordprefs = finale.FCChordPrefs()
     chordprefs:Load(1)
     local my_distance_result_flat = chordprefs:GetFlatBaselineAdjustment()
     local my_distance_result_sharp = chordprefs:GetSharpBaselineAdjustment()
     local my_distance_result_natural = chordprefs:GetNaturalBaselineAdjustment()
-    local my_distance = 5
     local chordprefs = finale.FCChordPrefs()
     chordprefs:Load(1)
     chordprefs:GetFlatBaselineAdjustment()
-    chordprefs.FlatBaselineAdjustment = my_distance + my_distance_result_flat
+    chordprefs.FlatBaselineAdjustment = config.vertical_increment + my_distance_result_flat
     chordprefs:GetSharpBaselineAdjustment()
-    chordprefs.SharpBaselineAdjustment = my_distance + my_distance_result_sharp
+    chordprefs.SharpBaselineAdjustment = config.vertical_increment + my_distance_result_sharp
     chordprefs:GetNaturalBaselineAdjustment()
-    chordprefs.NaturalBaselineAdjustment = my_distance + my_distance_result_natural
+    chordprefs.NaturalBaselineAdjustment = config.vertical_increment + my_distance_result_natural
     chordprefs:Save()
 end
 
