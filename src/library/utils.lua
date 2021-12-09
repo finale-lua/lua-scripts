4b54f8e635cca45223070a3d870be172117483b6
ThistleSifter <78799201+ThistleSifter@users.noreply.github.com>
2021-12-10 00:33:14 +1030

    Update mixins
    - Global mixins are now included automatically
    - Add support for mixins at any level in the class hierarchy
    - Add utils to library for general lua utility functions
    - Move copy_table to utils
    - Add some missing nil checks

---
+++ src/library/utils.lua
@@ -0,0 +1,31 @@
+--[[
+$module Utility Functions
+
+A library of general Lua utility functions.
+]]
+
+local utils = {}
+
+--[[
+% copy_table(t)
+
+If a table is passed, returns a copy, otherwise returns the passed value.
+
+@ t (mixed)
+: (mixed)
+]]
+function utils.copy_table(t)
+    if type(t) == 'table' then
+        local new = {}
+        for k, v in pairs(t) do
+            new[utils.copy_table(k)] = utils.copy_table(v)
+        end
+        setmetatable(new, utils.copy_table(getmetatable(t)))
+        return new
+    else
+        return t
+    end
+end
+
+
+return utils
