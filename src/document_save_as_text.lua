f083a645857d4ab22193337bdd9b1c9e0cfc53d0
Robert Patterson <robert@robertgpatterson.com>
2022-06-01 08:58:00 -0500

    basic expression support working

--- src/document_save_as_text.lua
+++ src/document_save_as_text.lua
@@ -6,6 +6,7 @@ function plugindef()
     finaleplugin.Version = "1.0"
     finaleplugin.Date = "May 30, 2022"
     finaleplugin.CategoryTags = "Document"
+    finaleplugin.MinJWLuaVersion = 0.63
     finaleplugin.Notes = [[
         This script writes the current document to a text file in a human readable format. The primary purpose is to find changes
         between one version of a document and another. The idea is to write each version out to a text file and then
@@ -17,6 +18,8 @@ end
 local text_extension = ".txt"
 
 local note_entry = require('library.note_entry')
+local expression = require('library.expression')
+local enigma_string = require('library.enigma_string')
 local mixin = require('library.mixin')
 
 local fcstr = function(str)
@@ -72,7 +75,7 @@ end
 
 function entry_string(entry)
     local retval = ""
-    -- ToDo: write entry-attached items (articulations done)
+    -- ToDo: write entry-attached items including lyrics syllables (articulations done)
     local articulations = entry:CreateArticulations()
     for articulation in each(articulations) do
         local articulation_def = articulation:CreateArticulationDef()
@@ -89,17 +92,45 @@ function entry_string(entry)
     return retval
 end
 
-function create_measure_table(measure_region)
+local get_edupos_table = function(measure_table, staff_number, edupos)
+    if not measure_table[staff_number] then
+        measure_table[staff_number] = {}
+    end
+    local staff_table = measure_table[staff_number]
+    if not staff_table[edupos] then
+        staff_table[edupos] = {}
+    end
+    return staff_table[edupos]
+end
+
+function create_measure_table(measure_region, measure)
     local measure_table = {}
+    -- ToDo: smart shapes, chords
+    local expression_assignments = measure:CreateExpressions()
+    for expression_assignment in each(expression_assignments) do
+        --require('mobdebug').start()
+        local staff_num = expression_assignment:CalcStaffInPageView()
+        if staff_num > 0 then
+            local edupos_table = get_edupos_table(measure_table, staff_num, expression_assignment.MeasurePos)
+            if not edupos_table.expressions then
+                edupos_table.expressions = {}
+            end
+            if expression.is_for_current_part(expression_assignment) then
+                if expression_assignment.Shape then
+                    local shapeexp_def = expression_assignment:CreateShapeExpressionDef()
+                    table.insert(edupos_table.expressions, " Shape "..tostring(shapeexp_def.ID))
+                else
+                    local textexp_def = expression_assignment:CreateTextExpressionDef()
+                    local exp_text = textexp_def:CreateTextString()
+                    enigma_string.expand_value_tag(exp_text, textexp_def:GetPlaybackTempoValue())
+                    exp_text:TrimEnigmaTags()
+                    table.insert(edupos_table.expressions, " " .. exp_text.LuaString)
+                end
+            end
+        end
+    end
     for entry in eachentry(measure_region) do
-        if not measure_table[entry.Staff] then
-            measure_table[entry.Staff] = {}
-        end
-        local staff_table = measure_table[entry.Staff]
-        if not staff_table[entry.MeasurePos] then
-            staff_table[entry.MeasurePos] = {}
-        end
-        local edupos_table = staff_table[entry.MeasurePos]
+        local edupos_table = get_edupos_table(measure_table, entry.Staff, entry.MeasurePos)
         if not edupos_table.entries then
             edupos_table.entries = {}
         end
@@ -120,12 +151,11 @@ function write_measure(file, measure, measure_number_regions)
     measure_region:SetFullDocument()
     measure_region.StartMeasure = measure.ItemNo
     measure_region.EndMeasure = measure.ItemNo
-    local measure_table = create_measure_table(measure_region)
+    local measure_table = create_measure_table(measure_region, measure)
     for slot = 1, measure_region.EndSlot do
         local staff_number = measure_region:CalcStaffNumber(slot)
         local staff_table = measure_table[staff_number]
         if staff_table then
-            --require('mobdebug').start()
             local staff = finale.FCCurrentStaffSpec()
             local staff_name = ""
             if staff:LoadForCell(finale.FCCell(measure.ItemNo, staff_number), 0) then
@@ -137,7 +167,12 @@ function write_measure(file, measure, measure_number_regions)
             file:write("  ", staff_name, ":")
             for edupos, edupos_table in pairsbykeys(staff_table) do
                 file:write(" ["..tostring(edupos).."]")
-                -- ToDo: write expressions, smart shapes first
+                -- ToDo: write smart shapes, chords first
+                if edupos_table.expressions then
+                    for _, exp_string in ipairs(edupos_table.expressions) do
+                        file:write(exp_string)
+                    end
+                end
                 if edupos_table.entries then
                     for _, entry_string in ipairs(edupos_table.entries) do
                         file:write(entry_string)
