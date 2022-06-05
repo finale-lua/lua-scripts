8078995a794acad1c5a09f57da5611934716c639
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2022-06-06 08:25:48 +1000

    Update notes_tie.lua
    With great help from Robert, finally a good working version that caters for V2 entries.

--- notes_tie.lua
+++ notes_tie.lua
@@ -1,13 +1,12 @@
 function plugindef()
     finaleplugin.RequireSelection = true
-    finaleplugin.Author = "Carl Vine"
-    finaleplugin.AuthorURL = "http://carlvine.com"
+    finaleplugin.Author = "Carl Vine and Robert Patterson"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
-    finaleplugin.Version = "v0.74"
-    finaleplugin.Date = "2022/06/04"
+    finaleplugin.Version = "v0.75"
+    finaleplugin.Date = "2022/06/06"
     finaleplugin.AdditionalMenuOptions = [[ Untie Notes ]]
-    finaleplugin.AdditionalUndoText = [[    Untie Notes ]]
-    finaleplugin.AdditionalPrefixes = [[    untie_notes = true ]]
+    finaleplugin.AdditionalUndoText = [[ Untie Notes ]]
+    finaleplugin.AdditionalPrefixes = [[ untie_notes = true ]]
     finaleplugin.AdditionalDescriptions = [[ Untie all notes in the selected region ]]
     finaleplugin.MinJWLuaVersion = 0.62
     finaleplugin.Notes = [[ 
@@ -20,50 +19,51 @@ end
 -- default to "tie" notes for normal operation
 untie_notes = untie_notes or false
 
-function tie_between_entries(entry_one, entry_two)
-    if not entry_one or not entry_two -- entries must exist
-        or entry_one:IsRest() or entry_two:IsRest() -- not rests
-        or entry_one.Voice2 ~= entry_two.Voice2 -- on same voice
-        or entry_two.GraceNote
-        then return
+local note_entry = require('library.note_entry')
+
+local function tied_to(note)
+    if not note then
+        return nil
     end
-    for note in each(entry_one) do
-        local note_tied_to = entry_two:FindPitch(note) -- find the pitch?
-        if note_tied_to then
-            note.Tie = true -- then tie it
-            note_tied_to.TieBackwards = true -- and backwards
+    local next_entry = note.Entry
+    if next_entry then
+        if next_entry.Voice2Launch then
+            next_entry = note_entry.get_next_same_v(next_entry)
+        else
+            next_entry = next_entry:Next()
+        end
+        if next_entry and next_entry:IsNote() and not next_entry.GraceNote then
+            local tied_to_note = next_entry:FindPitch(note)
+            if tied_to_note then
+                return tied_to_note
+            end
+            if next_entry.Voice2Launch then
+                local next_v2_entry = next_entry:Next()
+                tied_to_note = next_v2_entry:FindPitch(note)
+                if tied_to_note then
+                    return tied_to_note
+                end
+            end
         end
     end
+    return nil
 end
 
-function tie_notes_in_selection()
+local function tie_notes_in_selection()
     local region = finenv.Region()
 
     for staff_number = region.StartStaff, region.EndStaff do
         for layer_number = 0, 3 do  -- run through layers [0-based]
-            local saved_v1_entry = nil -- v1 entry held while v2 running
-            local v2_is_active = false
             local entry_layer = finale.FCNoteEntryLayer(layer_number, staff_number, region.StartMeasure, region.EndMeasure)
             entry_layer:Load()
-
             for entry in each(entry_layer) do
-                if not entry:Next() then -- this is the final entry
-                    if not entry.Voice2 and v2_is_active then -- possible left-over V1 ties
-                        tie_between_entries(saved_v1_entry, entry)
-                    end -- finished with this layer
-                else
-                    -- voice 1, launching V2
-                    if not entry.Voice2 and entry.Voice2Launch and entry:Next().Voice2 then -- voice 2 is launching
-                        saved_v1_entry = entry -- save V1 entry for later
-                        v2_is_active = true -- set V2 flag; nothing else to do
-                    else
-                        if not entry.Voice2 and v2_is_active then -- returning to V1 chain, so check saved_v1_entry
-                            tie_between_entries(saved_v1_entry, entry) -- check backwards tie to last saved V1 entry
-                            saved_v1_entry = nil -- clear the saved V1 entry
-                            v2_is_active = false -- clear V2 flag
+                if entry:IsNote() and region:IsEntryPosWithin(entry) then
+                    for note in each(entry) do
+                        local tied_to_note = tied_to(note)
+                        if tied_to_note and region:IsEntryPosWithin(tied_to_note.Entry) then
+                            note.Tie = true
+                            tied_to_note.TieBackwards = true
                         end
-                        -- check for normal forward tie
-                        tie_between_entries(entry, entry:Next()) -- tests for all compliance
                     end
                 end
             end
@@ -72,7 +72,7 @@ function tie_notes_in_selection()
     end
 end
 
-function untie_notes_in_selection()
+local function untie_notes_in_selection()
     for entry in eachentrysaved(finenv.Region()) do
         for note in each(entry) do
             note.Tie = false
