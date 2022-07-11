94e9a0aa78d86acc0e24a1d57834976b21ca1a2b
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2022-07-11 11:13:14 +1000

    Update cross_staff_offset.lua
    Got there at last. This works, including the vital saving of dialog position. But I can't preset those values to "nil" or it fails.

--- src/cross_staff_offset.lua
+++ src/cross_staff_offset.lua
@@ -1,10 +1,11 @@
 function plugindef()
     finaleplugin.RequireSelection = true
+    finaleplugin.HandlesUndo = true
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
     finaleplugin.Author = "Carl Vine"
-    finaleplugin.AuthorURL = "http://carlvine.com/?cv=lua"
-    finaleplugin.Version = "v1.28"
-    finaleplugin.Date = "2022/07/10"
+    finaleplugin.AuthorURL = "http://carlvine.com/lua/"
+    finaleplugin.Version = "v1.29"
+    finaleplugin.Date = "2022/07/11"
 	finaleplugin.MinJWLuaVersion = 0.62
     finaleplugin.AdditionalMenuOptions = [[  CrossStaff Offset No Dialog  ]]
     finaleplugin.AdditionalUndoText = [[     CrossStaff Offset No Dialog  ]]
@@ -29,20 +30,27 @@ end
 
 no_user_dialog = no_user_dialog or false
 
-local config = {
+-- other global variables for modeless operation
+global_dialog = nil
+global_dialog_options = { -- words, key value in config, edit control holder
+    { "Cross-staff offset:", "cross_staff_offset", nil },
+    { "Non-crossed offset:", "non_cross_offset", nil },
+    { "Layer 1-4 (0 = all):", "layer_number", nil }
+}
+config = {
     cross_staff_offset  = 0,
     non_cross_offset = 0,
     layer_number = 0,
-    window_pos_x = nil,
-    window_pos_y = nil
+    window_pos_x = 700,
+    window_pos_y = 200
 }
 local configuration = require("library.configuration")
 
 function is_out_of_range(horiz_offset)
-    return ( math.abs(horiz_offset) > 999 ) -- some unrealistic horizontal offset (EVPUs)
+    return ( math.abs(horiz_offset) > 999 ) -- some unreasonable EVPU offset
 end
 
-function is_error()
+function no_submission_errors()
     local error = ""
     if config.layer_number < 0 or config.layer_number > 4  then 
         error = "The layer number must\nbe between 0 and 4\n(not " .. config.layer_number .. ")"
@@ -52,17 +60,9 @@ function is_error()
     end
     if error ~= "" then  -- error dialog and exit
         finenv.UI():AlertNeutral("script: " .. plugindef(), error)
-        return true
+        return false
     end
-    return false
-end
-
-function change_the_offsets() -- change entry offsets in the chosen layer (0 = all layers)
-    for entry in eachentrysaved(finenv.Region(), config.layer_number) do
-        if entry:IsNote() then
-            entry.ManualPosition = entry.CrossStaff and config.cross_staff_offset or config.non_cross_offset
-        end
-    end
+    return true
 end
 
 function create_user_dialog() -- attempting MODELESS operation
@@ -76,12 +76,6 @@ function create_user_dialog() -- attempting MODELESS operation
     str.LuaString = plugindef()
     dialog:SetTitle(str)
 
-    local answer = {}
-    local texts = { -- words, default value
-        { "Cross-staff offset:", "cross_staff_offset" }, --> answer[1]
-        { "Non-crossed offset:", "non_cross_offset" }, --> answer[2]
-        { "Layer 1-4 (0 = all):", "layer_number" } --> answer[3]
-    }
     function make_static(msg, horiz, vert, width, color)
         local static = dialog:CreateStatic(horiz, vert)
         str.LuaString = msg
@@ -92,11 +86,11 @@ function create_user_dialog() -- attempting MODELESS operation
         end
     end
 
-    for i, v in ipairs(texts) do
+    for i, v in ipairs(global_dialog_options) do
         make_static(v[1], 0, current_vert, edit_box_horiz, "")
-        answer[i] = dialog:CreateEdit(edit_box_horiz, current_vert - mac_offset)
-        answer[i]:SetInteger(config[v[2]]) -- display the saved config value
-        answer[i]:SetWidth(75)
+        v[3] = dialog:CreateEdit(edit_box_horiz, current_vert - mac_offset)
+        v[3]:SetInteger(config[v[2]]) -- display the saved config value
+        v[3]:SetWidth(75)
         if i < 3 then
             make_static("EVPUs", edit_box_horiz + 80, current_vert, 75, "")
         end
@@ -104,44 +98,60 @@ function create_user_dialog() -- attempting MODELESS operation
     end
     make_static("cross to staff below = [ -24, 0 ] or [ -12, 12 ]", 0, current_vert + 8, 290, "red")
     make_static("cross to staff above = [ 24, 0 ] or [ 12, -12 ]", 0, current_vert + vertical_step, 290, "red")
-
-    if config.window_pos_x ~= nil and config.window_pos_y ~= nil then
-        dialog:StorePosition()
-        dialog:SetRestorePositionOnlyData(config.window_pos_x, config.window_pos_y)
-        dialog:RestorePosition()
-    end
-
-    function on_ok()
-        if not is_error() then
-            for i, v in ipairs(texts) do -- save the 3 integer values
-                config[v[2]] = answer[i]:GetInteger()
-            end
-            dialog:StorePosition()
-            config.window_pos_x = dialog.StoredX
-            config.window_pos_y = dialog.StoredY
-            configuration.save_user_settings("cross_staff_offset", config)
-            change_the_offsets()
-        end
-    end
-
     dialog:CreateOkButton()
     dialog:CreateCancelButton()
-    dialog:SetOkButtonCanClose(true)
-    dialog:RegisterHandleOkButtonPressed(on_ok)
---    dialog:RegisterCloseWindow(on_close)
     return dialog
 end
 
+function change_the_offsets() -- change entry offsets in the chosen layer (0 = all layers)
+    finenv.StartNewUndoBlock("CrossStaff Offset", false)
+    for entry in eachentrysaved(finenv.Region(), config.layer_number) do
+        if entry:IsNote() then
+            entry.ManualPosition = entry.CrossStaff and config.cross_staff_offset or config.non_cross_offset
+        end
+    end
+    if finenv.EndUndoBlock then
+        finenv.EndUndoBlock(true)
+        finenv.Region():Redraw()
+    else
+        finenv.StartNewUndoBlock("CrossStaff Offset", true)
+    end
+end
+
+function on_ok()
+    for i, v in ipairs(global_dialog_options) do -- save the 3 integer values
+        config[v[2]] = v[3]:GetInteger()
+    end
+    if no_submission_errors() then
+        change_the_offsets()
+    end
+end
+
+function on_close()
+    global_dialog:StorePosition()
+    config.window_pos_x = global_dialog.StoredX
+    config.window_pos_y = global_dialog.StoredY
+    configuration.save_user_settings("cross_staff_offset", config)
+end
+
 function cross_staff_offset()
-    configuration.get_user_settings("cross_staff_offset", config, true)
+    configuration.get_user_settings("cross_staff_offset", config)
     if no_user_dialog then
         change_the_offsets()
-    else
-        local dialog = create_user_dialog()
-        finenv.RegisterModelessDialog(dialog)
-        dialog:ShowModeless()
-        dialog:ExecuteModal(nil)
+        return
     end
+    global_dialog = create_user_dialog()
+    if config.window_pos_x ~= nil and config.window_pos_y ~= nil then
+        global_dialog:StorePosition()
+        global_dialog:SetRestorePositionOnlyData(config.window_pos_x, config.window_pos_y)
+        global_dialog:RestorePosition()
+    end
+    global_dialog:RegisterHandleOkButtonPressed(on_ok)
+    if global_dialog.RegisterCloseWindow then
+        global_dialog:RegisterCloseWindow(on_close)
+    end
+    finenv.RegisterModelessDialog(global_dialog)
+    global_dialog:ShowModeless()
 end
 
 cross_staff_offset()
