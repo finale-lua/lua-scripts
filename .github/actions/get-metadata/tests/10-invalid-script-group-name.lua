6a085ba20851269c547c4abb92c15ac0ef59593c
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-07-29 14:45:27 -0700

    parse new finaleplugin fields to script metadata

---
+++ .github/actions/get-metadata/tests/10-invalid-script-group-name.lua
@@ -0,0 +1,5 @@
+function plugindef()
+    finaleplugin.ScriptGroupName = "Hairpin creator"
+    finaleplugin.ScriptGroupDescription = "Create all sorts of hairpins"
+    return "Hairpin create crescendo", "Hairpin create crescendo", "Hairpin create crescendo"
+end
