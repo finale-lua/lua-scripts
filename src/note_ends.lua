3ad8c551928e9194b30eea3bec3e33883bc5f134
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2022-08-03 10:46:55 +1000

    Update note_ends.lua
    Like this??? The final if-elseif-end must surely remain aligned?

--- src/note_ends.lua
+++ src/note_ends.lua
@@ -66,14 +66,14 @@ function expand_note_ends()
                 or  (not is_compound_meter and not eighth_notes and start_beat % 2 == 0) -- special case quarter note on an odd beat
             )
             then
-            local duration_with_rest = entry.Duration + entry:Next().Duration
-            entry.Duration = note_value	-- expand target note
-            if duration_with_rest == note_value then
-                should_delete_next = true -- just delete the following rest
-            elseif duration_with_rest > note_value then -- some duration left over
-                entry:Next().Duration = duration_with_rest - note_value -- make rest smaller
-                should_delete_next = false -- and don't delete it
-            end
+                local duration_with_rest = entry.Duration + entry:Next().Duration
+                entry.Duration = note_value	-- expand target note
+                if duration_with_rest == note_value then
+                    should_delete_next = true -- just delete the following rest
+                elseif duration_with_rest > note_value then -- some duration left over
+                    entry:Next().Duration = duration_with_rest - note_value -- make rest smaller
+                    should_delete_next = false -- and don't delete it
+                end
         end
     end
 end
