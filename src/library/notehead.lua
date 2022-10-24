6a64c085f5e90899ff597439abfbc96519f65620
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2022-10-24 11:21:05 +1100

    Update notehead.lua
    Adding a family of "wedge" shapes, and removed redundant test for shape = "number"

--- src/library/notehead.lua
+++ src/library/notehead.lua
@@ -18,6 +18,7 @@ local config = {
     x_normal = 192,
     x_circled = 192, -- Maestro has no option for the "long" crossed notehead
     x_breve = 192,
+    x_breve_resize = 120,
     triangle_up_hollow = 177,
     triangle_down_hollow = 198,
     triangle_up_filled = 209,
@@ -32,11 +33,20 @@ local config = {
     square_half = 173,
     square_whole = 194,
     square_breve = 221,
+    wedge_filled = 108,
+    wedge_hollow = 231,
+    wedge_whole = 231,
+    wedge_breve = 231,
+    wedge_hollow_resize = 100,
+    wedge_whole_resize = 100,
+    wedge_breve_resize = 100,
+    wedge_whole_offset = -14,
+    wedge_breve_offset = -14,
     hidden = 202,
     default_notehead = 207,
 }
 
--- allowable names for noteheads
+-- allowable names for notehead families
 local notehead_shape_names = {
     x = true,
     diamond = true,
@@ -44,6 +54,7 @@ local notehead_shape_names = {
     triangle = true,
     slash = true,
     square = true,
+    wedge = true,
     hidden = true,
 }
 
@@ -56,6 +67,7 @@ if library.is_font_smufl_font() then
     config.x_normal = 0xe0a9
     config.x_circled = 0xe0ec
     config.x_breve = 0xe0b4 -- not perfect but best in Finale Maestro
+    config.x_breve_resize = 120
     config.triangle_up_hollow = 0xe0bd
     config.triangle_down_hollow = 0xe0c6
     config.triangle_up_filled = 0xe0be
@@ -65,13 +77,22 @@ if library.is_font_smufl_font() then
     config.triangle_up_breve = 0xe0ed
     config.triangle_down_breve = 0xe0f1
     config.slash_quarter = 0xe100
-    config.slash_half = 0xe101
+    config.slash_half = 0xe103
     config.slash_whole = 0xe102
     config.slash_breve = 0xe10a
     config.square_quarter = 0xe934
     config.square_half = 0xe935
     config.square_whole = 0xe937
     config.square_breve = 0xe933
+    config.wedge_filled = 0xe1c5
+    config.wedge_hollow = 0xe1c8
+    config.wedge_whole = 0xe1c4
+    config.wedge_breve = 0xe1ca
+    config.wedge_hollow_resize = 120
+    config.wedge_whole_resize = 120
+    config.wedge_breve_resize = 120
+    config.wedge_whole_offset = 0
+    config.wedge_breve_offset = 0
     config.hidden = 0xe0a5
     config.default_notehead = 0xe0a4
 end
@@ -81,10 +102,10 @@ configuration.get_parameters("notehead.config.txt", config)
 --[[
 % change_shape
 
-Changes the given notehead to a specified notehead descriptor string. Currently only supports "diamond".
+Changes the given notehead to a specified notehead descriptor string. 
 
 @ note (FCNote)
-@ shape (lua string)
+@ shape (lua string) or (number)
 
 : (FCNoteheadMod) the new notehead mod record created
 ]]
@@ -92,6 +113,7 @@ function notehead.change_shape(note, shape)
     local notehead = finale.FCNoteheadMod()
     notehead:EraseAt(note)
     local notehead_char = config.default_notehead
+
     if type(shape) == "number" then -- specific character GLYPH requsted, not notehead "family"
         notehead_char = shape
         shape = "number"
@@ -106,73 +128,88 @@ function notehead.change_shape(note, shape)
         local duration = entry.Duration
         local offset = 0
         local resize = 100
-        if shape ~= "number" then
-            if shape == "hidden" then
-                notehead_char = config.hidden
-            --  --------
-            elseif string.find(shape, "diamond") then
-                notehead_char = config.diamond_hollow
-                resize = config.diamond_resize
+
+        -- shape == "number" requires no further modification
+        if shape == "hidden" then
+            notehead_char = config.hidden
+        --  --------
+        elseif string.find(shape, "diamond") then
+            notehead_char = config.diamond_hollow
+            resize = config.diamond_resize
+            if duration >= finale.BREVE then
+                offset = config.diamond_breve_offset
+                notehead_char = config.diamond_breve
+            elseif duration >= finale.WHOLE_NOTE then
+                offset = config.diamond_whole_offset
+                notehead_char = config.diamond_whole
+            elseif duration < finale.HALF_NOTE and string.find(shape, "guitar") then
+                notehead_char = config.diamond_filled
+            end
+        --  --------
+        elseif shape == "slash" then
+            notehead_char = config.slash_quarter
+            if duration >= finale.BREVE then
+                notehead_char = config.slash_breve
+            elseif duration >= finale.WHOLE_NOTE then
+                notehead_char = config.slash_whole
+            elseif duration >= finale.HALF_NOTE then
+                notehead_char = config.slash_half
+            end
+        --  --------
+        elseif shape == "square" then
+            notehead_char = config.square_quarter
+            if duration >= finale.BREVE then
+                notehead_char = config.square_breve
+            elseif duration >= finale.WHOLE_NOTE then
+                notehead_char = config.square_whole
+            elseif duration >= finale.HALF_NOTE then
+                notehead_char = config.square_half
+            end
+        --  --------
+        elseif shape == "wedge" then
+            notehead_char = config.wedge_filled
+            if duration >= finale.BREVE then
+                notehead_char = config.wedge_breve
+                resize = config.wedge_breve_resize
+                offset = config.wedge_breve_offset
+            elseif duration >= finale.WHOLE_NOTE then
+                notehead_char = config.wedge_whole
+                resize = config.wedge_whole_resize
+                offset = config.wedge_whole_offset
+            elseif duration >= finale.HALF_NOTE then
+                notehead_char = config.wedge_hollow
+                resize = config.wedge_hollow_resize
+        end
+        --  --------
+        elseif shape == "x" then
+            notehead_char = (duration >= finale.HALF_NOTE) and config.x_circled or config.x_normal
+            if duration >= finale.BREVE then
+                notehead_char = config.x_breve
+                resize = config.x_breve_resize
+            end
+        --  --------
+        elseif shape == "triangle" then
+            if entry:CalcStemUp() then
+                notehead_char = config.triangle_down_filled
                 if duration >= finale.BREVE then
-                    offset = config.diamond_breve_offset
-                    notehead_char = config.diamond_breve
+                    notehead_char = config.triangle_down_breve
                 elseif duration >= finale.WHOLE_NOTE then
-                    offset = config.diamond_whole_offset
-                    notehead_char = config.diamond_whole
-                elseif duration < finale.HALF_NOTE and string.find(shape, "guitar") then
-                    notehead_char = config.diamond_filled
-                end
-            --  --------
-            elseif shape == "slash" then
-                notehead_char = config.slash_quarter
-                if duration >= finale.BREVE then
-                    notehead_char = config.slash_breve
-                elseif duration >= finale.WHOLE_NOTE then
-                    notehead_char = config.slash_whole
+                    notehead_char = config.triangle_down_whole
                 elseif duration >= finale.HALF_NOTE then
-                    notehead_char = config.slash_half
+                    notehead_char = config.triangle_down_hollow
                 end
-            --  --------
-            elseif shape == "square" then
-                notehead_char = config.square_quarter
+            else
+                notehead_char = config.triangle_up_filled
                 if duration >= finale.BREVE then
-                    notehead_char = config.square_breve
+                    notehead_char = config.triangle_up_breve
                 elseif duration >= finale.WHOLE_NOTE then
-                    notehead_char = config.square_whole
+                    notehead_char = config.triangle_up_whole
                 elseif duration >= finale.HALF_NOTE then
-                    notehead_char = config.square_half
-                end
-                --  --------
-            elseif shape == "x" then
-                notehead_char = (duration >= finale.HALF_NOTE) and config.x_circled or config.x_normal
-                if duration >= finale.BREVE then
-                    notehead_char = config.x_breve
-                    resize = 120
-                end
-            --  --------
-            elseif shape == "triangle" then
-                if entry:CalcStemUp() then
-                    notehead_char = config.triangle_down_filled
-                    if duration >= finale.BREVE then
-                        notehead_char = config.triangle_down_breve
-                    elseif duration >= finale.WHOLE_NOTE then
-                        notehead_char = config.triangle_down_whole
-                    elseif duration >= finale.HALF_NOTE then
-                        notehead_char = config.triangle_down_hollow
-                    end
-                else
-                    notehead_char = config.triangle_up_filled
-                    if duration >= finale.BREVE then
-                        notehead_char = config.triangle_up_breve
-                    elseif duration >= finale.WHOLE_NOTE then
-                        notehead_char = config.triangle_up_whole
-                    elseif duration >= finale.HALF_NOTE then
-                        notehead_char = config.triangle_up_hollow
-                    end
+                    notehead_char = config.triangle_up_hollow
                 end
             end
         end
-        --  all done testing notehead family --
+        --  finished testing notehead family --
         notehead.CustomChar = notehead_char
         if resize ~= 100 then
             notehead.Resize = resize
