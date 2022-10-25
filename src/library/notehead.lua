d9574b80f75596ba6adb6780bc831ea8f6813f57
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2022-10-26 10:19:10 +1100

    Update notehead.lua
    Substantial revision moving config format to multi-layered table, simplifying shape selection switch.

--- src/library/notehead.lua
+++ src/library/notehead.lua
@@ -1,100 +1,127 @@
 --[[
 $module Notehead
 ]] --
+-- version cv0.51 2022/10/26
+
 local notehead = {}
-
 local configuration = require("library.configuration")
-
 local library = require("library.general_library")
 
 local config = {
-    diamond_hollow = 79,
-    diamond_filled = 226,
-    diamond_whole = 79, -- Maestro lacks alternate diamond shapes
-    diamond_breve = 79,
-    diamond_resize = 110,
-    diamond_whole_offset = 5,
-    diamond_breve_offset = 14,
-    x_normal = 192,
-    x_circled = 192, -- Maestro has no option for the "long" crossed notehead
-    x_breve = 192,
-    x_breve_resize = 120,
-    triangle_up_hollow = 177,
-    triangle_down_hollow = 198,
-    triangle_up_filled = 209,
-    triangle_down_filled = 224,
-    triangle_up_breve = 177,
-    triangle_down_breve = 198,
-    slash_quarter = 243,
-    slash_half = 203,
-    slash_whole = 213,
-    slash_breve = 213,
-    square_quarter = 208,
-    square_half = 173,
-    square_whole = 194,
-    square_breve = 221,
-    wedge_filled = 108,
-    wedge_hollow = 231,
-    wedge_whole = 231,
-    wedge_breve = 231,
-    wedge_hollow_resize = 100,
-    wedge_whole_resize = 100,
-    wedge_breve_resize = 100,
-    wedge_whole_offset = -14,
-    wedge_breve_offset = -14,
-    hidden = 202,
-    default_notehead = 207,
-}
-
--- allowable names for notehead families
-local notehead_shape_names = {
-    x = true,
-    diamond = true,
-    guitar_diamond = true, -- diamonds are CLOSED for quarter note or shorter
-    triangle = true,
-    slash = true,
-    square = true,
-    wedge = true,
-    hidden = true,
+    diamond = {
+        quarter = { glyph = 79, size = 110 },
+        half  = { glyph = 79, size = 110 },
+        whole = { glyph = 79, size = 110, offset = 5 },
+        breve = { glyph = 79, size = 110, offset = 14 },
+    },
+    diamond_guitar = {
+        quarter = { glyph = 226, size = 110 },
+        half  = { glyph = 79, size = 110 },
+        whole = { glyph = 79, size = 110, offset = 5 },
+        breve = { glyph = 79, size = 110, offset = 14 },
+    },
+    x = {
+        quarter = { glyph = 192 },
+        half  = { glyph = 192 },
+        whole = { glyph = 192 },
+        breve = { glyph = 192, size = 120 },
+    },
+    triangle = {     -- triangle "up" glyphs
+        quarter = { glyph = 209 },
+        half  = { glyph = 177 },
+        whole = { glyph = 177 },
+        breve = { glyph = 177 },
+    },
+    triangle_down = { -- special case!
+        quarter = { glyph = 224 },
+        half  = { glyph = 198 },
+        whole = { glyph = 198 },
+        breve = { glyph = 198 },
+    },
+    slash = {
+        quarter = { glyph = 243 },
+        half  = { glyph = 203 },
+        whole = { glyph = 213 },
+        breve = { glyph = 213 },
+    },
+    square = {
+        quarter = { glyph = 208 },
+        half  = { glyph = 173 },
+        whole = { glyph = 194 },
+        breve = { glyph = 221 },
+    },
+    wedge = {
+        quarter = { glyph = 108 },
+        half  = { glyph = 231 },
+        whole = { glyph = 231, offset = -14 },
+        breve = { glyph = 231, offset = -14 },
+    },
+    hidden = {
+        quarter = { glyph = 202 },
+        half  = { glyph = 202 },
+        whole = { glyph = 202 },
+        breve = { glyph = 202 },
+    }
 }
 
 -- Default to SMuFL characters for SMuFL font (without needing a config file)
 if library.is_font_smufl_font() then
-    config.diamond_filled = 0xe0e2
-    config.diamond_hollow = 0xe0e1
-    config.diamond_whole = 0xe0d8
-    config.diamond_breve = 0xe0d7
-    config.x_normal = 0xe0a9
-    config.x_circled = 0xe0ec
-    config.x_breve = 0xe0b4 -- not perfect but best in Finale Maestro
-    config.x_breve_resize = 120
-    config.triangle_up_hollow = 0xe0bd
-    config.triangle_down_hollow = 0xe0c6
-    config.triangle_up_filled = 0xe0be
-    config.triangle_down_filled = 0xe0c7
-    config.triangle_up_whole = 0xe0ef
-    config.triangle_down_whole = 0xe0f3
-    config.triangle_up_breve = 0xe0ed
-    config.triangle_down_breve = 0xe0f1
-    config.slash_quarter = 0xe100
-    config.slash_half = 0xe103
-    config.slash_whole = 0xe102
-    config.slash_breve = 0xe10a
-    config.square_quarter = 0xe934
-    config.square_half = 0xe935
-    config.square_whole = 0xe937
-    config.square_breve = 0xe933
-    config.wedge_filled = 0xe1c5
-    config.wedge_hollow = 0xe1c8
-    config.wedge_whole = 0xe1c4
-    config.wedge_breve = 0xe1ca
-    config.wedge_hollow_resize = 120
-    config.wedge_whole_resize = 120
-    config.wedge_breve_resize = 120
-    config.wedge_whole_offset = 0
-    config.wedge_breve_offset = 0
-    config.hidden = 0xe0a5
-    config.default_notehead = 0xe0a4
+    config = {
+        diamond = {
+            quarter = { glyph = 0xe0e1, size = 110 },
+            half  = { glyph = 0xe0e1, size = 110 },
+            whole = { glyph = 0xe0d8, size = 110 },
+            breve = { glyph = 0xe0d7, size = 110 },
+        },
+        diamond_guitar = {
+            quarter = { glyph = 0xe0e2, size = 110 },
+            half  = { glyph = 0xe0e1, size = 110 },
+            whole = { glyph = 0xe0d8, size = 110 },
+            breve = { glyph = 0xe0d7, size = 110 },
+        },
+        x = {
+            quarter = { glyph = 0xe0a9 },
+            half  = { glyph = 0xe0ec },
+            whole = { glyph = 0xe0ec },
+            breve = { glyph = 0xe0b4, size = 120 },
+        },
+        triangle = {     -- triangle "up" glyphs
+            quarter = { glyph = 0xe0be },
+            half  = { glyph = 0xe0bd },
+            whole = { glyph = 0xe0ef },
+            breve = { glyph = 0xe0ed },
+        },
+        triangle_down = { -- special case ... triangle "down" glyphs
+            quarter = { glyph = 0xe0c7 },
+            half  = { glyph = 0xe0c6 },
+            whole = { glyph = 0xe0f3 },
+            breve = { glyph = 0xe0f1 },
+        },
+        slash = {
+            quarter = { glyph = 0xe100 },
+            half  = { glyph = 0xe103 },
+            whole = { glyph = 0xe102 },
+            breve = { glyph = 0xe10a },
+        },
+        square = {
+            quarter = { glyph = 0xe934 },
+            half  = { glyph = 0xe935 },
+            whole = { glyph = 0xe937 },
+            breve = { glyph = 0xe933 },
+        },
+        wedge = {
+            quarter = { glyph = 0xe1c5 },
+            half  = { glyph = 0xe1c8, size = 120 },
+            whole = { glyph = 0xe1c4, size = 120, offset = -14 },
+            breve = { glyph = 0xe1ca, size = 120, offset = -14 },
+        },
+        hidden = {
+            quarter = { glyph = 0xe0a5 },
+            half  = { glyph = 0xe0a5 },
+            whole = { glyph = 0xe0a5 },
+            breve = { glyph = 0xe0a5 },
+        },
+    }
 end
 
 configuration.get_parameters("notehead.config.txt", config)
@@ -112,13 +139,13 @@ Changes the given notehead to a specified notehead descriptor string.
 function notehead.change_shape(note, shape)
     local notehead = finale.FCNoteheadMod()
     notehead:EraseAt(note)
-    local notehead_char = config.default_notehead
+    local notehead_char
 
-    if type(shape) == "number" then -- specific character GLYPH requsted, not notehead "family"
+    if type(shape) == "number" then -- specific character GLYPH requested, not notehead "family"
         notehead_char = shape
         shape = "number"
-    else -- check for valid shape identifier
-        shape = notehead_shape_names[shape] and shape or "default"
+    elseif not config[shape] then
+        shape = "default" -- unrecognised shape name or "default" requested
     end
 
     if shape == "default" then
@@ -129,86 +156,29 @@ function notehead.change_shape(note, shape)
         local offset = 0
         local resize = 100
 
-        -- shape == "number" requires no further modification
-        if shape == "hidden" then
-            notehead_char = config.hidden
-        --  --------
-        elseif string.find(shape, "diamond") then
-            notehead_char = config.diamond_hollow
-            resize = config.diamond_resize
+        if shape ~= "number" then -- "number" is a specific glyph that needs no further modification
+            local note_type = "quarter"
             if duration >= finale.BREVE then
-                offset = config.diamond_breve_offset
-                notehead_char = config.diamond_breve
+                note_type = "breve"
             elseif duration >= finale.WHOLE_NOTE then
-                offset = config.diamond_whole_offset
-                notehead_char = config.diamond_whole
-            elseif duration < finale.HALF_NOTE and string.find(shape, "guitar") then
-                notehead_char = config.diamond_filled
-            end
-        --  --------
-        elseif shape == "slash" then
-            notehead_char = config.slash_quarter
-            if duration >= finale.BREVE then
-                notehead_char = config.slash_breve
-            elseif duration >= finale.WHOLE_NOTE then
-                notehead_char = config.slash_whole
+                note_type = "whole"
             elseif duration >= finale.HALF_NOTE then
-                notehead_char = config.slash_half
+                note_type = "half"
             end
-        --  --------
-        elseif shape == "square" then
-            notehead_char = config.square_quarter
-            if duration >= finale.BREVE then
-                notehead_char = config.square_breve
-            elseif duration >= finale.WHOLE_NOTE then
-                notehead_char = config.square_whole
-            elseif duration >= finale.HALF_NOTE then
-                notehead_char = config.square_half
+
+            local ref_table = config[shape][note_type]
+            if shape == "triangle" and entry:CalcStemUp() then
+                ref_table = config["triangle_down"][note_type]
             end
-        --  --------
-        elseif shape == "wedge" then
-            notehead_char = config.wedge_filled
-            if duration >= finale.BREVE then
-                notehead_char = config.wedge_breve
-                resize = config.wedge_breve_resize
-                offset = config.wedge_breve_offset
-            elseif duration >= finale.WHOLE_NOTE then
-                notehead_char = config.wedge_whole
-                resize = config.wedge_whole_resize
-                offset = config.wedge_whole_offset
-            elseif duration >= finale.HALF_NOTE then
-                notehead_char = config.wedge_hollow
-                resize = config.wedge_hollow_resize
-        end
-        --  --------
-        elseif shape == "x" then
-            notehead_char = (duration >= finale.HALF_NOTE) and config.x_circled or config.x_normal
-            if duration >= finale.BREVE then
-                notehead_char = config.x_breve
-                resize = config.x_breve_resize
+            notehead_char = ref_table.glyph
+            if ref_table.size then
+                resize = ref_table.size
             end
-        --  --------
-        elseif shape == "triangle" then
-            if entry:CalcStemUp() then
-                notehead_char = config.triangle_down_filled
-                if duration >= finale.BREVE then
-                    notehead_char = config.triangle_down_breve
-                elseif duration >= finale.WHOLE_NOTE then
-                    notehead_char = config.triangle_down_whole
-                elseif duration >= finale.HALF_NOTE then
-                    notehead_char = config.triangle_down_hollow
-                end
-            else
-                notehead_char = config.triangle_up_filled
-                if duration >= finale.BREVE then
-                    notehead_char = config.triangle_up_breve
-                elseif duration >= finale.WHOLE_NOTE then
-                    notehead_char = config.triangle_up_whole
-                elseif duration >= finale.HALF_NOTE then
-                    notehead_char = config.triangle_up_hollow
-                end
+            if ref_table.offset then
+                offset = ref_table.offset
             end
         end
+
         --  finished testing notehead family --
         notehead.CustomChar = notehead_char
         if resize ~= 100 then
