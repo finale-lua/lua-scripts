07aff7b4863d5e5ac0a8b1ed816953fca3e77009
Robert Patterson <robert@robertgpatterson.com>
2023-01-11 15:17:32 -0600

    dialog box working

--- src/staff_split_layers.lua
+++ src/staff_split_layers.lua
@@ -1,5 +1,6 @@
 function plugindef()
-    finaleplugin.RequireSelection = true
+    finaleplugin.RequireSelection = false
+    finaleplugin.HandlesUndo = true -- not recognized by JW Lua or RGP Lua v0.55
     finaleplugin.Author = "Robert Patterson"
     finaleplugin.AuthorURL = "https://www.robertgpatterson.com"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
@@ -10,16 +11,12 @@ function plugindef()
         Multiple measures and staves can be selected at once. 
         Articulations on the original are optionally copied to the other layer.
     ]]
-    return "Staff Split Layers", "Staff Split Layers", "Split chords from one layer 1 into two independent layers, based on a split point."
+    return "Staff Split Layers...", "Staff Split Layers", "Split chords from one layer 1 into two independent layers, based on a split point."
 end
 
 local note_entry = require("library.note_entry")
 local layer = require("library.layer")
-
-local split_from_top = 2
-local source_layer = 1
-local destination_layer = 2
-local clone_artics = true
+local mixin = require("library.mixin")
 
 function explode_one_slot(slot)
     local region = finale.FCMusicRegion()
@@ -28,6 +25,11 @@ function explode_one_slot(slot)
     region.EndSlot = slot
     region:SetStartMeasurePosLeft()
     region:SetEndMeasurePosRight()
+
+    local split_from_top = global_dialog:GetControl("split_after"):GetInteger()
+    local source_layer = global_dialog:GetControl("from_layer"):GetInteger()
+    local destination_layer = global_dialog:GetControl("to_layer"):GetInteger()
+    local clone_artics = 0 ~= global_dialog:GetControl("clone_artics"):GetCheck()
 
     local start_measure = region.StartMeasure
     local end_measure = region.EndMeasure
@@ -57,14 +59,77 @@ function explode_one_slot(slot)
     end
 end
 
-function staff_split_layers()
+function do_staff_split_layers()
     local region = finale.FCMusicRegion()
     if not region:SetCurrentSelection() then
-        return -- no selection, which should be impossible due to plugindef() setting
+        return -- no selection
     end
+    local undostr = "Split Layers " .. tostring(finenv.Region().StartMeasure)
+    if finenv.Region().StartMeasure ~= finenv.Region().EndMeasure then
+        undostr = undostr .. " - " .. tostring(finenv.Region().EndMeasure)
+    end
+    finenv.StartNewUndoBlock(undostr, false) -- this works on both JW Lua and RGP Lua
     for slot = region.StartSlot, region.EndSlot do
         explode_one_slot(slot)
     end
+    if finenv.EndUndoBlock then -- EndUndoBlock only exists on RGP Lua 0.56 and higher
+        finenv.EndUndoBlock(true)
+        finenv.Region():Redraw()
+    else
+        finenv.StartNewUndoBlock(undostr, true) -- JW Lua automatically terminates the final undo block we start here
+    end
+end
+
+function create_dialog_box()
+    local dialog = mixin.FCXCustomLuaWindow():SetTitle("Split Layers")
+    local current_y = 0
+    local x_increment = 130
+    local y_increment = 22
+    local edit_width = 30
+    -- from layer
+    dialog:CreateStatic(0, current_y + 2)
+        :SetWidth(x_increment - 5)
+        :SetText("From Layer (1-4):")
+    local edit_x = x_increment + (finenv.UI():IsOnMac() and 4 or 0)
+    dialog:CreateEdit(edit_x, current_y, "from_layer")
+        :SetWidth(edit_width)
+        :SetInteger(1)
+    current_y = current_y + y_increment
+    -- to layer
+    dialog:CreateStatic(0, current_y + 2)
+        :SetWidth(x_increment - 5)
+        :SetText("To Layer (1-4):")
+    local edit_x = x_increment + (finenv.UI():IsOnMac() and 4 or 0)
+    dialog:CreateEdit(edit_x, current_y, "to_layer")
+        :SetWidth(edit_width)
+        :SetInteger(2)
+    current_y = current_y + y_increment
+    -- notes to keep
+    dialog:CreateStatic(0, current_y + 2)
+        :SetWidth(x_increment - 5)
+        :SetText("Split After (From Top):")
+    local edit_x = x_increment + (finenv.UI():IsOnMac() and 4 or 0)
+    dialog:CreateEdit(edit_x, current_y, "split_after")
+        :SetWidth(edit_width)
+        :SetInteger(1)
+    current_y = current_y + y_increment
+    -- clone articulations
+    dialog:CreateCheckbox(0, current_y + 2, "clone_artics"):SetText("Copy Articulations")
+        :SetWidth(x_increment)
+        :SetCheck(1)
+    -- ok/cancel
+    dialog:CreateOkButton()
+    dialog:CreateCancelButton()
+    dialog:RegisterHandleOkButtonPressed(function(self)
+            do_staff_split_layers()
+        end
+    )
+    return dialog
+end
+
+function staff_split_layers()
+    global_dialog = global_dialog or create_dialog_box()
+    global_dialog:RunModeless()
 end
 
 staff_split_layers()
