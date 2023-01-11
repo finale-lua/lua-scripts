f93f8795e64f43ba1e4fcd18608bd5900cca24ea
Robert Patterson <robert@robertgpatterson.com>
2023-01-11 15:37:59 -0600

    error checking and cleanup

--- src/staff_split_layers.lua
+++ src/staff_split_layers.lua
@@ -10,6 +10,14 @@ function plugindef()
         Chords from the source layers in the selected region are split into another layer on the same staff based on a split point. 
         Multiple measures and staves can be selected at once. 
         Articulations on the original are optionally copied to the other layer.
+        
+        The dialog box has the following options:
+        
+        - From Layer (1-4): the source layer to split from (defaults to 1)
+        - To Layer (1-4): the target layer to split to (defaults to 2)
+        - Split After (From Top): the number of chord tones to preserve in the source layer, counting from the top of each chord. All other notes are split to the target layer.
+        - Copy Articulations: if checked, copies articulations from the source to the target.
+        
     ]]
     return "Staff Split Layers...", "Staff Split Layers", "Split chords from one layer 1 into two independent layers, based on a split point."
 end
@@ -18,18 +26,13 @@ local note_entry = require("library.note_entry")
 local layer = require("library.layer")
 local mixin = require("library.mixin")
 
-function explode_one_slot(slot)
+function explode_one_slot(slot, split_from_top, source_layer, destination_layer, clone_artics)
     local region = finale.FCMusicRegion()
     region:SetCurrentSelection()
     region.StartSlot = slot
     region.EndSlot = slot
     region:SetStartMeasurePosLeft()
     region:SetEndMeasurePosRight()
-
-    local split_from_top = global_dialog:GetControl("split_after"):GetInteger()
-    local source_layer = global_dialog:GetControl("from_layer"):GetInteger()
-    local destination_layer = global_dialog:GetControl("to_layer"):GetInteger()
-    local clone_artics = 0 ~= global_dialog:GetControl("clone_artics"):GetCheck()
 
     local start_measure = region.StartMeasure
     local end_measure = region.EndMeasure
@@ -59,18 +62,41 @@ function explode_one_slot(slot)
     end
 end
 
-function do_staff_split_layers()
+function do_staff_split_layers()    
+    local split_from_top = global_dialog:GetControl("split_after"):GetInteger()
+    local source_layer = global_dialog:GetControl("from_layer"):GetInteger()
+    local destination_layer = global_dialog:GetControl("to_layer"):GetInteger()
+    local clone_artics = 0 ~= global_dialog:GetControl("clone_artics"):GetCheck()
+    
+    if source_layer < 1 or source_layer > layer.max_layers() then
+        finenv.UI():AlertError("Invalid From Layer.", "Layer Number")
+        return
+    end
+    if destination_layer < 1 or destination_layer > layer.max_layers() then
+        finenv.UI():AlertError("Invalid To Layer.", "Layer Number")
+        return
+    end
+    if source_layer == destination_layer then
+        finenv.UI():AlertError("From and To layers must be different.", "Layer Number")
+        return
+    end
+    if split_from_top <= 0 then
+        finenv.UI():AlertError("Split After must be greater than zero.", "Split After")
+        return
+    end
     local region = finale.FCMusicRegion()
     if not region:SetCurrentSelection() then
+        finenv.UI():AlertError("Please select music before executing this script.", "No Selection")
         return -- no selection
     end
+
     local undostr = "Split Layers " .. tostring(finenv.Region().StartMeasure)
     if finenv.Region().StartMeasure ~= finenv.Region().EndMeasure then
         undostr = undostr .. " - " .. tostring(finenv.Region().EndMeasure)
     end
     finenv.StartNewUndoBlock(undostr, false) -- this works on both JW Lua and RGP Lua
     for slot = region.StartSlot, region.EndSlot do
-        explode_one_slot(slot)
+        explode_one_slot(slot, split_from_top, source_layer, destination_layer, clone_artics)
     end
     if finenv.EndUndoBlock then -- EndUndoBlock only exists on RGP Lua 0.56 and higher
         finenv.EndUndoBlock(true)
