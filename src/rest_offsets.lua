fef06813c87ffc6fee09abc5c37cd2f314bb5422
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2023-10-30 09:16:23 +1100

    Update rest_offsets.lua
    Substantial rewrite that adds key action equivalents, config replaces RetainState, “smart” layer number handling, better documentation.

--- src/rest_offsets.lua
+++ src/rest_offsets.lua
@@ -3,77 +3,122 @@ function plugindef()
     finaleplugin.Author = "Carl Vine"
     finaleplugin.AuthorURL = "https://carlvine.com/lua/"
     finaleplugin.Copyright = "https://creativecommons.org/licenses/by/4.0/"
-    finaleplugin.Version = "v1.52"
-    finaleplugin.Date = "2023/07/26"
+    finaleplugin.Version = "v1.57"
+    finaleplugin.Date = "2023/10/30"
     finaleplugin.Notes = [[ 
         This script alters the vertical position of rests. 
         It duplicates Finale's inbuilt "Move Rests..." plug-in but with less mouse activity. 
         It is also a quick way to reset rest positions in every layer, the default setting.
 
         New rests are "floating" and will avoid entries in other layers (if present) 
-        using the setting for "Adjust Floating Rests by..." at Finale → Document → Document Options → Layers.  
-        This script can stop rests "floating", instead "fixing" them to a specific offset from the default position. 
-        On transposing staves such "fixed" rests will behave like actual notes and change position 
-        if "Display in Concert Pitch" is selected. 
-        Set the "Floating Rests" checkbox to return all rests on the chosen layer to "floating".
+        using the setting for "Adjust Floating Rests by..." at 
+        Finale → Document → Document Options → Layers.  
+        This script can stop them "floating", instead "fixing" them 
+        to a specific offset from the default position. 
+        On transposing staves these "fixed" rests will behave like notes 
+        and change position if "Display in Concert Pitch" is selected.
 
-        A "space" is the vertical distance between staff lines, and a "step" is half a space. 
-        The distance between the top and bottom lines of a 5-line staff is 4 spaces or 8 steps. 
-        Rests usually "centre" on the middle staff line, 4 steps below the top line of a 5-line staff. 
-        This script, like Finale, uses "step" offsets to shift rests relative to the default position. 
+        Hit the "f" key or select the "Floating Rests" checkbox to return all rests on 
+        the chosen layer to "floating". 
+        Hit the "q" key to view these notes. 
+        To repeat the same action as before without a confirmation dialog, 
+        hold down the SHIFT key when starting the script.
+
+        == INFO ==
+
+        A Space is the vertical distance between staff lines, and a Step is half a Space. 
+        The distance between the top and bottom lines of a 5-line staff is 4 Spaces or 8 Steps. 
+        Rests usually "centre" on the middle staff line, 4 Steps below the top line of a 5-line staff. 
+        This script, like Finale, shifts rests by Steps relative to the default position.
     ]]
     return "Rest Offsets...", "Rest Offsets", "Change the vertical offset of rests by layer"
 end
 
--- RetainLuaState retains one global:
-config = config or {
+local config = {
     offset = 0,
     layer = 0,
     make_floating = 0,
     pos_x = false,
     pos_y = false
 }
+local configuration = require("library.configuration")
 local mixin = require("library.mixin")
 local layer = require("library.layer")
 local note_entry = require("library.note_entry")
+local script_name = "rest_offsets"
 
-function no_errors()
-    local max = layer.max_layers()
-    local msg = ""
-    if math.abs(config.offset) > 20 then
-        msg = "Offset level must be reasonable, say between -20 and 20 (not " .. config.offset .. ")\n\n"
+local function dialog_set_position(dialog)
+    if config.window_pos_x and config.window_pos_y then
+        dialog:StorePosition()
+        dialog:SetRestorePositionOnlyData(config.window_pos_x, config.window_pos_y)
+        dialog:RestorePosition()
     end
-    if config.layer < 0 or config.layer > max then
-        msg = msg .. "Layer number must be an integer\nbetween 0 and " .. max .. " (not " .. config.layer .. ")"
-    end
-    if msg ~= "" then
+end
+
+local function dialog_save_position(dialog)
+    dialog:StorePosition()
+    config.window_pos_x = dialog.StoredX
+    config.window_pos_y = dialog.StoredY
+    configuration.save_user_settings(script_name, config)
+end
+
+local function no_errors()
+    local max_step = 48 -- (2 inches)
+    if math.abs(config.offset) > max_step then
+        local msg = "Rest offset value must be reasonable, say +/- "
+        .. max_step .. " steps or less ...\nnot " .. config.offset .. " steps"
         finenv.UI():AlertInfo(msg, "User Error")
         return false
     end
     return true
 end
 
-function make_dialog()
-    local x = 110
+local function user_chooses()
+    local max = layer.max_layers()
+    local notes = finaleplugin.Notes:gsub(" %s+", " "):gsub("\n ", "\n"):sub(2)
+    local function show_info() finenv.UI():AlertInfo(notes, "About " .. plugindef()) end
+    local x = 122
     local y_grid = { 15, 45, 70 }
     local x_off = finenv.UI():IsOnMac() and 3 or 0 -- extra y-offset for Mac Edit box
     local dialog = mixin.FCXCustomLuaWindow():SetTitle(plugindef())
+    local edit = {}
+    local saved = { offset = config.offset, layer = config.layer, float = config.make_floating }
 
-    local stat = dialog:CreateStatic(0, y_grid[1]):SetText("Vertical offset:")
-        :SetWidth(x):SetEnable(config.make_floating == 0)
-    local offset = dialog:CreateEdit(x, y_grid[1] - x_off):SetInteger(config.offset)
-        :SetWidth(50):SetEnable(config.make_floating == 0)
-    dialog:CreateStatic(0, y_grid[2]):SetText("Layer 1-" .. layer.max_layers() .. " (0 = all)"):SetWidth(x)
-    local layer_num = dialog:CreateEdit(x, y_grid[2] - x_off):SetInteger(config.layer):SetWidth(50)
+    edit.title = dialog:CreateStatic(0, y_grid[1]):SetText("Vertical offset (steps)")
+        :SetWidth(x):SetEnable(saved.float == 0)
+    edit.offset = dialog:CreateEdit(x, y_grid[1] - x_off):SetInteger(config.offset)
+        :SetWidth(50):SetEnable(saved.float == 0)
+    dialog:CreateStatic(0, y_grid[2]):SetText("Layer 1-" .. max .. " (0 = all)"):SetWidth(x)
+    edit.layer = dialog:CreateEdit(x, y_grid[2] - x_off):SetInteger(config.layer):SetWidth(50)
 
-    local float = dialog:CreateCheckbox(0, y_grid[3]):SetText("Floating Rests")
+        local function offset_enabled()
+            local n = edit.float:GetCheck()
+            edit.title:SetEnable(n == 0)
+            edit.offset:SetEnable(n == 0)
+            edit[(n == 0) and "offset" or "layer"]:SetKeyboardFocus()
+        end
+        local function key_check(id)
+            local s = edit[id]:GetText():lower()
+            if s:find("[^-0-9]") or (id == "layer" and s:find("[-5-9]")) then
+                if s:find("[?q]") then show_info()
+                elseif s:find("f") then -- toggle "float" checkbox
+                    edit.float:SetCheck((edit.float:GetCheck() == 0) and 1 or 0)
+                    offset_enabled()
+                end
+                edit[id]:SetText(saved[id])
+            else
+                if id == "layer" then
+                    s = s:sub(-1)
+                    edit[id]:SetText(s)
+                end
+                saved[id] = edit[id]:GetText()
+            end
+        end
+    edit.float = dialog:CreateCheckbox(0, y_grid[3]):SetText("Floating Rests (f)")
         :SetCheck(config.make_floating):SetWidth(x * 2)
-        :AddHandleCommand(function(self)
-            offset:SetEnable(self:GetCheck() == 0)
-            stat:SetEnable(self:GetCheck() == 0)
-        end)
+        :AddHandleCommand(function() offset_enabled() end)
 
-    texts = { -- offset number / x offset / description /  vertical position
+    local texts = { -- offset number / x offset / description /  vertical position
         {  "4", 5, "= top staff line",    0 },
         {  "0", 5, "= middle staff line", 15 },
         { "-4", 0, "= bottom staff line", 30 },
@@ -83,49 +128,39 @@ function make_dialog()
         dialog:CreateStatic(x + 60 + v[2], v[4]):SetText(v[1])
         dialog:CreateStatic(x + 75, v[4]):SetText(v[3]):SetWidth(x)
     end
+    edit.offset:AddHandleCommand(function() key_check("offset") end)
+    edit.layer:AddHandleCommand(function() key_check("layer") end)
+
     dialog:CreateButton(x * 2 + 45, y_grid[3]):SetText("?"):SetWidth(20)
-        :AddHandleCommand(function()
-            finenv.UI():AlertInfo(finaleplugin.Notes:gsub(" %s+", " "), "About " .. plugindef())
-        end)
+        :AddHandleCommand(function() show_info() end)
     dialog:CreateOkButton()
     dialog:CreateCancelButton()
+    dialog_set_position(dialog)
     dialog:RegisterHandleOkButtonPressed(function(self)
-        config.offset = offset:GetInteger()
-        config.layer = layer_num:GetInteger()
-        config.make_floating = float:GetCheck()
+        config.offset = edit.offset:GetInteger()
+        config.layer = edit.layer:GetInteger()
+        config.make_floating = edit.float:GetCheck()
+        dialog_save_position(self)
     end)
-    dialog:RegisterCloseWindow(function(self)
-        self:StorePosition()
-        config.pos_x = self.StoredX
-        config.pos_y = self.StoredY
-    end)
-    return dialog
+    dialog:RegisterCloseWindow(function(self) dialog_save_position(self) end)
+    return (dialog:ExecuteModal(nil) == finale.EXECMODAL_OK)
 end
 
-function make_the_change()
-    if finenv.RetainLuaState ~= nil then
-        finenv.RetainLuaState = true
-    end
-    for entry in eachentrysaved(finenv.Region(), config.layer) do
-        if entry:IsRest() then
-            if (config.make_floating == 1) then
-                entry:SetFloatingRest(true)
-            else
-                note_entry.rest_offset(entry, config.offset)
+local function change_rest_offset()
+    configuration.get_user_settings(script_name, config, true)
+    local shift_key = finenv.QueryInvokedModifierKeys and
+        (finenv.QueryInvokedModifierKeys(finale.CMDMODKEY_ALT)
+         or finenv.QueryInvokedModifierKeys(finale.CMDMODKEY_SHIFT))
+    if shift_key or (user_chooses() and no_errors()) then
+        for entry in eachentrysaved(finenv.Region(), config.layer) do
+            if entry:IsRest() then
+                if (config.make_floating == 1) then
+                    entry:SetFloatingRest(true)
+                else
+                    note_entry.rest_offset(entry, config.offset)
+                end
             end
         end
-    end
-end
-
-function change_rest_offset()
-    local dialog = make_dialog()
-    if config.pos_x and config.pos_y then
-        dialog:StorePosition()
-            :SetRestorePositionOnlyData(config.pos_x, config.pos_y)
-            :RestorePosition()
-    end
-    if dialog:ExecuteModal(nil) == finale.EXECMODAL_OK and no_errors() then
-        make_the_change()
     end
 end
 
