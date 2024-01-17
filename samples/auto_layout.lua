bf6cdb32c5946a16914efe6076190306b9b12a05
Robert Patterson <robert@robertgpatterson.com>
2024-01-16 21:03:49 -0600

    sample auto_layout updates

--- samples/auto_layout.lua
+++ samples/auto_layout.lua
@@ -19,36 +19,45 @@ function create_dialog()
     local y = 0
 
     local ctrl_label = dlg:CreateStatic(0, y)
-    ctrl_label.AutoResizeWidth = true
+    ctrl_label:DoAutoResizeWidth(true)
     ctrl_label:SetWidth(0)
     ctrl_label:SetText(finale.FCString("Label:"))
-    local ctrl_edit = dlg:CreateEdit(0, y - win_mac(5, 3))
-    ctrl_edit:SetText("Editable")
-    ctrl_edit:AssureNoHorizontalOverlap(ctrl_label, 0)
+    local ctrl_edit = dlg:CreateEdit(0, y - win_mac(2, 3))
+    ctrl_edit:SetText(finale.FCString("Editable"))
+    ctrl_edit:AssureNoHorizontalOverlap(ctrl_label, 2)
     y = y + 20
 
     local ctrl_checkbox = dlg:CreateCheckbox(0, y)
-    ctrl_checkbox:SetAutoResizeWidth(true)
+    ctrl_checkbox:DoAutoResizeWidth(true)
     ctrl_checkbox:SetWidth(0)
     ctrl_checkbox:SetText(finale.FCString("Short."))
     y = y + 20
 
-    local ctrl_edit = dlg:CreateEdit(0, y)
-    ctrl_edit:SetAutoResizeWidth(true)
-    ctrl_edit:SetWidth(0)
-    ctrl_edit:SetText(finale.FCString("Short."))
-    --ctrl_edit:SetMeasurement(1, finale.MEASUREMENTUNIT_DEFAULT)
+    local ctrl_edit2 = dlg:CreateEdit(0, y)
+    ctrl_edit2:DoAutoResizeWidth(true)
+    ctrl_edit2:SetWidth(0)
+    ctrl_edit2:SetText(finale.FCString("Short."))
+    --ctrl_edit2:SetMeasurement(1, finale.MEASUREMENTUNIT_DEFAULT)
     y = y + 30
 
     local ctrl_button = dlg:CreateButton(0, 70)
-    ctrl_button:SetAutoResizeWidth(true)
+    ctrl_button:DoAutoResizeWidth(true)
     ctrl_button:SetWidth(0)
     ctrl_button:SetText(finale.FCString("Short."))
     y = y + 30
 
-    local ctrl_popup = dlg:CreatePopup(0, y)
-    ctrl_popup:SetAutoResizeWidth(true)
+    local popup_label = dlg:CreateStatic(0, y)
+    popup_label:DoAutoResizeWidth(true)
+    popup_label:SetWidth(0)
+    popup_label:SetText(finale.FCString("Popup:"))
+    local ctrl_popup = dlg:CreatePopup(0, y - win_mac(2, 2))
+    ctrl_popup:DoAutoResizeWidth(true)
     ctrl_popup:SetWidth(0)
+    ctrl_popup:AssureNoHorizontalOverlap(popup_label, 2)
+    --ctrl_popup:HorizontallyAlignWith(ctrl_edit)
+    for k, v in pairs(finale.FCControl.__propget) do
+        print (tostring(k), tostring(v), tostring(ctrl_popup[k]))
+    end
     for counter = 1, 3 do
         if counter == 3 then
             ctrl_popup:AddString(finale.FCString("This is long option text " .. counter .. "."))
@@ -56,12 +65,21 @@ function create_dialog()
             ctrl_popup:AddString(finale.FCString("Short " .. counter .. "."))
         end
     end
-    ctrl_popup:SetSelectedItem(2)
-    y = y + 20
+    ctrl_popup:SetSelectedItem(1)
+    y = y + 22
 
-    local ctrl_cbobox = dlg:CreateComboBox(0, y)
-    ctrl_cbobox:SetAutoResizeWidth(true)
+    local cbobox_label = dlg:CreateStatic(0, y)
+    cbobox_label:DoAutoResizeWidth(true)
+    cbobox_label:SetWidth(0)
+    cbobox_label:SetText(finale.FCString("ComboBox:"))
+    local ctrl_cbobox = dlg:CreateComboBox(0, y - win_mac(2, 4))
+    ctrl_cbobox:DoAutoResizeWidth(true)
     ctrl_cbobox:SetWidth(40)
+    ctrl_cbobox:AssureNoHorizontalOverlap(cbobox_label, 2)
+    ctrl_cbobox:HorizontallyAlignWith(ctrl_popup)
+    for k, v in pairs(finale.FCControl.__propget) do
+        print (tostring(k), tostring(v), tostring(ctrl_cbobox[k]))
+    end
     for counter = 1, 3 do
         if counter == 3 then
             ctrl_cbobox:AddString(finale.FCString("This is long option text " .. counter .. "."))
@@ -76,7 +94,7 @@ function create_dialog()
     local counter = 1
     for rbtn in each(ctrl_radiobuttons) do
         rbtn:SetWidth(0)
-        rbtn.AutoResizeWidth = true
+        rbtn:DoAutoResizeWidth(true)
         if counter == 2 then
             rbtn:SetText(finale.FCString("This is long option text " .. counter .. "."))
         else
@@ -97,3 +115,4 @@ end
 
 global_dialog = global_dialog or create_dialog()
 global_dialog:RunModeless()
+--global_dialog:ExecuteModal(nil)
