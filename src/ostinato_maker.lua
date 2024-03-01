415da2e9f9ed1c99eeebab6e951bb318244ffd9d
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2024-03-01 11:23:25 +1100

    Update ostinato_maker.lua
    Converted to modal-modeless User Option; plugin.Notes changed to `utils.show_notes_dialog()`; `script_name` now derived from `general_library`.

--- src/ostinato_maker.lua
+++ src/ostinato_maker.lua
@@ -4,68 +4,67 @@ function plugindef()
     finaleplugin.Author = "Carl Vine after Michael McClennan & Jacob Winkler"
     finaleplugin.AuthorURL = "https://carlvine.com/lua/"
     finaleplugin.Copyright = "https://creativecommons.org/licenses/by/4.0/"
-    finaleplugin.Version = "v0.19"
-    finaleplugin.Date = "2024/01/31"
-    finaleplugin.MinJWLuaVersion = 0.62
+    finaleplugin.Version = "0.23" -- MODAL/MODELESS optional
+    finaleplugin.Date = "2024/03/01"
+    finaleplugin.MinJWLuaVersion = 0.70
     finaleplugin.Notes = [[
         Copy the current selection and paste it consecutively 
         to the right a nominated number of times. 
         The replicas can span barlines ignoring time signatures. 
-        The same effect can be achieved with Edit → Paste Multiple 
+        The same effect can be achieved with _Edit_ → _Paste Multiple_, 
         but this script is simpler to use and works intuitively 
         on the current music selection in a single step. 
 
-        To repeat the last action without confirmation 
-        dialog hold down [shift] when starting the script. 
-        Choose to independently include or remove articulations, 
+        To repeat the last action without a confirmation 
+        dialog hold down [Shift] when starting the script. 
+        Independently include or remove articulations, 
         expressions, smartshapes, lyrics or chords from the repeats. 
+        Your choice at _Finale_ → _Settings..._ → _Edit_ → _Automatic Music Spacing_ 
+        determines whether or not the music is _respaced_ on completion. 
 
-        This script grew from the "region_replicate_music.lua" script in 
-        the FinaleLua.com repository by Michael McClennan and Jacob Winkler.
+        Select __Modeless__ if you prefer the dialog window to 
+        "float" above your score so you can change the score selection 
+        while the script is active. In this mode, click __Apply__ [Return/Enter] 
+        to create an ostinato and __Cancel__ [Escape] to close the window. 
+        Cancelling __Modeless__ will apply the _next_ time you use the script.
+
+        This script grew out of the _region_replicate_music.lua_ script 
+        by Michael McClennan and Jacob Winkler in the 
+        [FinaleLua.com](https://FinaleLua.com) repository. 
+
+        > __Key Commands__: 
+
+        > - __q__: show these script notes 
+        > - __w__: flip [copy Articulations] 
+        > - __e__: flip [copy Expressions] 
+        > - __r__: flip [copy Smartshapes] 
+        > - __t__: flip [copy Lyrics] 
+        > - __y__: flip [copy Chords]  
+        > - __a__: copy all 
+        > - __z__: copy none 
+        > - __m__: flip [Modeless] 
     ]]
     return "Ostinato Maker...", "Ostinato Maker",
         "Copy the current selection and paste it consecutively to the right a number of times"
 end
 
-local info_notes = [[
-Copy the current selection and paste it consecutively
-to the right a nominated number of times.
-The replicas can span barlines ignoring time signatures.
-The same effect can be achieved with Edit → Paste Multiple
-but this script is simpler to use and works intuitively
-on the current music selection in a single step.
-**
-To repeat the last action without confirmation
-dialog hold down [shift] when starting the script.
-Choose to independently include or remove articulations,
-expressions, smartshapes, lyrics or chords from the repeats.
-**
-This script grew from the "region_replicate_music.lua" script in
-the FinaleLua.com repository by Michael McClennan and Jacob Winkler.
-**
-Key Commands:
-*• q @t show these script notes
-*• w @t flip [copy articulations]
-*• e @t flip [copy expressions]
-*• r @t flip [copy smartshapes]
-*• t @t flip [copy lyrics]
-*• y @t flip [copy chords]
-*– – –
-*• a @t copy all
-*• z @t copy none
-]]
-info_notes = info_notes:gsub("\n%s*", " "):gsub("*", "\n"):gsub("@t", "\t")
-    .. "\n(" .. finaleplugin.Version .. ")"
-
 local configuration = require("library.configuration")
 local mixin = require("library.mixin")
+local utils = require("library.utils")
 local library = require("library.general_library")
 local script_name = library.calc_script_name()
-local dialog, current_selection
+local refocus_document = false -- set to true if utils.show_notes_dialog is used
+local selection
+local saved_bounds = {}
+local bounds = { -- primary region selection boundaries
+    "StartStaff", "StartMeasure", "StartMeasurePos",
+    "EndStaff",   "EndMeasure",   "EndMeasurePos",
+}
 
 local config = {
     num_repeats  = 1,
-    timer_id     = 1,
+    timer_id    = 1,
+    modeless    = false, -- false = modal / true = modeless
     window_pos_x = false,
     window_pos_y = false,
 }
@@ -75,20 +74,7 @@ local dialog_options = { -- and populate config values (unchecked)
 }
 for _, v in ipairs(dialog_options) do config[v] = 0 end -- (default unchecked)
 
-local bounds = { -- primary region selection boundary properties
-    "StartStaff", "StartMeasure", "StartMeasurePos",
-    "EndStaff",   "EndMeasure",   "EndMeasurePos",
-}
-
-local function copy_region_bounds()
-    local copy = {}
-    for _, v in ipairs(bounds) do
-        copy[v] = finenv.Region()[v]
-    end
-    return copy
-end
-
-local function dialog_set_position()
+local function dialog_set_position(dialog)
     if config.window_pos_x and config.window_pos_y then
         dialog:StorePosition()
         dialog:SetRestorePositionOnlyData(config.window_pos_x, config.window_pos_y)
@@ -96,7 +82,7 @@ local function dialog_set_position()
     end
 end
 
-local function dialog_save_position()
+local function dialog_save_position(dialog)
     dialog:StorePosition()
     config.window_pos_x = dialog.StoredX
     config.window_pos_y = dialog.StoredY
@@ -108,26 +94,36 @@ local function measure_duration(measure_number)
     return m:Load(measure_number) and m:GetDuration() or 0
 end
 
-local function selection_id()
-    if finenv.Region():IsEmpty() then return " - no selection - " end
-    local rgn = finenv.Region()
-    local ratio = rgn.StartMeasure + (rgn.StartMeasurePos / measure_duration(rgn.StartMeasure))
-    local id = "m" .. string.format("%.2f", ratio) .. "-"
-    local m = measure_duration(rgn.EndMeasure)
-    ratio = rgn.EndMeasure + (math.min(rgn.EndMeasurePos, m) / m)
-    id = id .. "m" .. string.format("%.2f", ratio)
-    return id
+local function get_staff_name(staff_num)
+    local staff = finale.FCStaff()
+    staff:Load(staff_num)
+    local str = staff:CreateDisplayFullNameString().LuaString
+    if not str or str == "" then
+        str = "Staff " .. staff_num
+    end
+    return str
 end
 
-local function staff_id()
-    if finenv.Region():IsEmpty() then return "" end
-    local staff = finale.FCStaff()
-    staff:Load(finenv.Region().StartStaff)
-    local str = staff:CreateDisplayFullNameString()
-    local id = "Staff: " .. str.LuaString .. " → "
-    staff:Load(finenv.Region().EndStaff)
-    str = staff:CreateDisplayFullNameString()
-    return id .. str.LuaString
+local function initialise_parameters()
+    -- set_saved_bounds
+    local rgn = finenv.Region()
+    for _, property in ipairs(bounds) do
+        saved_bounds[property] = rgn[property]
+    end
+    -- update_selection_id
+    selection = { staff = "no staff", region = "no selection"} -- default
+    if not rgn:IsEmpty() then
+        -- measures
+        local r1 = rgn.StartMeasure + (rgn.StartMeasurePos / measure_duration(rgn.StartMeasure))
+        local m = measure_duration(rgn.EndMeasure)
+        local r2 = rgn.EndMeasure + (math.min(rgn.EndMeasurePos, m) / m)
+        selection.region = string.format("m%.2f-m%.2f", r1, r2)
+        -- staves
+        selection.staff = get_staff_name(rgn.StartStaff)
+        if rgn.EndStaff ~= rgn.StartStaff then
+            selection.staff = selection.staff .. " → " .. get_staff_name(rgn.EndStaff)
+        end
+    end
 end
 
 local function add_duration(measure_number, position, add_edu)
@@ -266,108 +262,94 @@ local function region_erasures(rgn)
     end
 end
 
-local function paste_many_copies()
-    if finenv.Region():IsEmpty() then
-        finenv.UI():AlertError("Please select some music before\n"
-            .. "running this script.", plugindef() .. " Error" )
-        return
-    end
-    local rgn = finenv.Region()
-    local origin = copy_region_bounds() -- copy current bounds
-    local undo_str = "Ostinato Maker " .. selection_id()
-    finenv.StartNewUndoBlock(undo_str, false)
-    --  ---- DO THE WORK
+local function paste_many_copies(region)
+    local rgn = mixin.FCMMusicRegion()
+    rgn:SetRegion(region)
+    finenv.StartNewUndoBlock("Ostinato " .. selection.region .. " x " .. config.num_repeats,
+        false)
     local m_d = measure_duration(rgn.EndMeasure)
     if rgn.EndMeasurePos >= m_d then
         rgn.EndMeasurePos = m_d - 1
     end
     rgn:CopyMusic() -- save a copy of the current selection
     local duration = region_duration(rgn)
-    local first_rpt = nil -- save start position of first repeat
-    for _ = 1, config.num_repeats do
+    local first_step
+    for i = 1, config.num_repeats do
         if not shift_region_by_EDU(rgn, duration) then break end
         rgn:PasteMusic()
-        if not first_rpt then -- only save the first repeat region
-            first_rpt = { measure = rgn.StartMeasure, pos = rgn.StartMeasurePos }
+        if i == 1 then -- save start of the first repeat region
+            first_step = { m = rgn.StartMeasure, pos = rgn.StartMeasurePos }
         end
     end
     rgn:ReleaseMusic()
     -- erase unwanted markings across whole ostinato passage
-    rgn.StartMeasure = first_rpt.measure
-    rgn.StartMeasurePos = first_rpt.pos
-    region_erasures(rgn)
-    -- reset to start of operation
-    for _, v in ipairs(bounds) do rgn[v] = origin[v] end
+    if first_step then
+        rgn.StartMeasure = first_step.m
+        rgn.StartMeasurePos = first_step.pos
+    end
+    region_erasures(rgn)  -- erase markings from subset of full region
+    rgn:SetRegion(region) -- reset to original selection
     rgn:SetInDocument()
-    --  ----
-    if finenv.EndUndoBlock then
-        finenv.EndUndoBlock(true)
-        finenv.Region():Redraw()
-    else
-        finenv.StartNewUndoBlock(undo_str, true)
-    end
+    finenv.EndUndoBlock(true)
+    finenv.Region():Redraw()
 end
 
-local function on_timer()
-    local changed = false
-    for _, v in ipairs(bounds) do
-        if current_selection[v] ~= finenv.Region()[v] then
-            changed = true
-            break
-        end
-    end
-    if changed then
-        current_selection = copy_region_bounds()
-        dialog:GetControl("info")
-            :SetText("Selection " .. selection_id() .. "\n" .. staff_id())
-    end
-end
-
-local function create_dialog()
+local function run_user_dialog()
     local edit_x, e_wide, y_step = 110, 40, 18
     local y_offset = finenv.UI():IsOnMac() and 3 or 0
     local save_rpt = config.num_repeats
-    local name = plugindef():gsub("%.%.%.", "")
-    dialog = mixin.FCXCustomLuaWindow():SetTitle(name)
+    local name = plugindef():sub(1, -4)
+    local dialog = mixin.FCXCustomLuaWindow():SetTitle(name)
     local y = 0
-    local function flip_check(id)
-        local ctl = dialog:GetControl(dialog_options[id])
-        ctl:SetCheck((ctl:GetCheck() + 1) % 2)
-    end
-    local function check_all_state(state)
-        for _, v in ipairs(dialog_options) do
-            dialog:GetControl(v):SetCheck(state)
+    --
+        local function flip_check(id)
+            local ctl = dialog:GetControl(dialog_options[id])
+            ctl:SetCheck((ctl:GetCheck() + 1) % 2)
         end
-    end
-    local function info_dialog()
-        finenv.UI():AlertInfo(info_notes, "About " .. name)
-    end
-    local function key_check(ctl) -- some stray key commands
-        local s = ctl:GetText():lower()
-        if s:find("[^0-9]") then
-            if s:find("q") then info_dialog()
-            elseif s:find("w") then flip_check(1)
-            elseif s:find("e") then flip_check(2)
-            elseif s:find("r") then flip_check(3)
-            elseif s:find("t") then flip_check(4)
-            elseif s:find("y") then flip_check(5)
-            elseif s:find("a") then check_all_state(1)
-            elseif s:find("z") then check_all_state(0)
+        local function check_all_state(state)
+            for _, v in ipairs(dialog_options) do
+                dialog:GetControl(v):SetCheck(state)
+            end
+        end
+        local function info_dialog()
+            utils.show_notes_dialog(dialog, "About " .. name)
+            refocus_document = true
+        end
+        local function key_check(ctl) -- some stray key commands
+            local s = ctl:GetText():lower()
+            if s:find("[^0-9]") then
+                if s:find("q") then info_dialog()
+                elseif s:find("w") then flip_check(1)
+                elseif s:find("e") then flip_check(2)
+                elseif s:find("r") then flip_check(3)
+                elseif s:find("t") then flip_check(4)
+                elseif s:find("y") then flip_check(5)
+                elseif s:find("m") then
+                    local mod = dialog:GetControl("modeless")
+                    mod:SetCheck((mod:GetCheck() + 1) % 2)
+                elseif s:find("a") then check_all_state(1)
+                elseif s:find("z") then check_all_state(0)
+                end
+            else
+                s = s:sub(-2) -- 2-digit limit
+                save_rpt = s
             end
             ctl:SetText(save_rpt):SetKeyboardFocus()
-        else
-            s = s:sub(-3) -- 3-digit limit
-            ctl:SetText(s)
-            save_rpt = s
         end
-    end
-    dialog:CreateStatic(0, y, "info")
-        :SetText("Selection " .. selection_id() .. "\n" .. staff_id())
-        :SetWidth(edit_x * 2):SetHeight(30)
-    y = y + 35
+        local function on_timer() -- look for changes in selected region
+            for k, v in pairs(saved_bounds) do
+                if finenv.Region()[k] ~= v then -- selection changed
+                    initialise_parameters() -- update selection tracker
+                    dialog:GetControl("info1"):SetText(selection.staff)
+                    dialog:GetControl("info2"):SetText(selection.region)
+                    break -- all done
+                end
+            end
+        end
+    --
     dialog:CreateStatic(0, y):SetText("Repeat ostinato:"):SetWidth(edit_x)
     dialog:CreateStatic(edit_x, y):SetText("Include:"):SetWidth(90)
-    y = y + y_step
+    y = y + y_step + 2
     local num_repeats = dialog:CreateEdit(0, y + 2 - y_offset)
         :SetWidth(e_wide - 4):SetText(config.num_repeats)
         :AddHandleCommand(function(self) key_check(self) end)
@@ -377,35 +359,73 @@ local function create_dialog()
            :SetText(v:sub(6, -1):gsub("^%l", string.upper)):SetWidth(90)
         y = y + y_step
     end
-    local q = dialog:CreateButton(0, y - 19):SetText("?"):SetWidth(20)
+    dialog:CreateCheckbox(0, y, "modeless"):SetWidth(80)
+        :SetCheck(config.modeless and 1 or 0):SetText("\"Modeless\" Dialog")
+    local q = dialog:CreateButton(edit_x + 70, y):SetText("?"):SetWidth(20)
         :AddHandleCommand(function() info_dialog() end)
-    dialog:CreateOkButton()
+    -- modeless selection info
+    if config.modeless then
+        y = y + 15
+        dialog:CreateStatic(20, y, "info1"):SetText(selection.staff):SetWidth(edit_x + 70)
+        y = y + 15
+        dialog:CreateStatic(20, y, "info2"):SetText(selection.region):SetWidth(edit_x + 70)
+    end
+
+    dialog:CreateOkButton():SetText(config.modeless and "Apply" or "OK")
     dialog:CreateCancelButton()
-    dialog_set_position()
-    dialog:RegisterHandleTimer(on_timer)
-    dialog:RegisterInitWindow(function()
+    dialog_set_position(dialog)
+    if config.modeless then dialog:RegisterHandleTimer(on_timer) end
+    dialog:RegisterInitWindow(function(self)
+        dialog:SetOkButtonCanClose(not config.modeless)
+        if config.modeless then self:SetTimer(config.timer_id, 125) end
         q:SetFont(q:CreateFontInfo():SetBold(true))
         num_repeats:SetKeyboardFocus()
-        dialog:SetTimer(config.timer_id, 125)
     end)
-    dialog:RegisterCloseWindow(function()
-        dialog_save_position()
-        dialog:StopTimer(config.timer_id)
-    end)
+    local change_mode = false
     dialog:RegisterHandleOkButtonPressed(function()
         config.num_repeats = num_repeats:GetInteger()
         for _, v in ipairs(dialog_options) do
             config[v] = dialog:GetControl(v):GetCheck()
         end
-        paste_many_copies()
+        paste_many_copies(finenv.Region())
     end)
+    dialog:RegisterCloseWindow(function(self)
+        if config.modeless then self:StopTimer(config.timer_id) end
+        local mode = (dialog:GetControl("modeless"):GetCheck() == 1)
+        change_mode = (mode and not config.modeless) -- modal -> modeless?
+        config.modeless = mode
+        dialog_save_position(self)
+    end)
+    if config.modeless then   -- "modeless"
+        dialog:RunModeless()
+    else
+        dialog:ExecuteModal() -- "modal"
+        if refocus_document then finenv.UI():ActivateDocumentWindow() end
+    end
+    return change_mode
 end
 
 local function make_ostinato()
     configuration.get_user_settings(script_name, config, true)
-    current_selection = copy_region_bounds()
-    create_dialog()
-    dialog:RunModeless()
+    if not config.modelss and finenv.Region():IsEmpty() then
+        finenv.UI():AlertError(
+            "Please select some music before\nrunning this script", plugindef()
+        )
+        return
+    end
+
+    local qim = finenv.QueryInvokedModifierKeys
+    local mod_key = qim and (qim(finale.CMDMODKEY_ALT) or qim(finale.CMDMODKEY_SHIFT))
+    local mode_change = true
+
+    initialise_parameters()
+    if mod_key then
+        paste_many_copies(finenv.Region())
+    else
+        while mode_change do
+            mode_change = run_user_dialog()
+        end
+    end
 end
 
 make_ostinato()
