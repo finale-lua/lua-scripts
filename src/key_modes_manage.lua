59ade585a0a5e3aae97cd39388b36e4a32e4c275
Robert Patterson <robert@robertgpatterson.com>
2024-03-21 11:12:46 -0500

    moving ahead with keymode editor

--- src/key_modes_manage.lua
+++ src/key_modes_manage.lua
@@ -13,15 +13,22 @@ end
 
 -- luacheck: ignore 11./global_dialog
 
+local utils = require("library.utils")
 local mixin = require("library.mixin")
+
+context = context or
+{
+    global_timer_id = 1,
+    current_doc = 0,
+    current_keymodes = finale.FCCustomKeyModeDefs(),
+    current_selection = 0
+}
 
 local key_mode_types =
 {
     "Predefined",
     "Linear",
-    "Nonlinear",
-    "Microtone",
-    "Other"
+    "Nonlinear"
 }
 
 local linear_mode_types =
@@ -61,32 +68,19 @@ local function calc_key_mode_desc(key_mode)
     local diatonic_steps = #key:CalcDiatonicStepsMap()
     local chromatic_steps = key:CalcTotalChromaticSteps()
     if chromatic_steps == 0 then chromatic_steps = 12 end
-    local function get_type()
-        if key_mode:IsPredefined() then
-            return 1
-        end
-        if diatonic_steps ~= 7 then
-            return 5
-        end
-        if chromatic_steps ~= 12 then
-            return 4
-        end
-        return key_mode:IsLinear() and 2 or 3
-    end
-    local tonal_center = key_mode.TonalCenters[0] or 0
-    local mode_type = key_mode_types[get_type()]
+    local tonal_center = key_mode.BaseTonalCenter
+    local retval = "[" .. (key_mode.ItemNo & ~0xc000) .. "]"
     if key:IsMajor() then
-        return mode_type .. " Major"
+        return retval .. " Predefined Major"
     elseif key:IsMinor() then
-        return mode_type .. " Minor"
+        return retval " Predefined Minor"
     end
-    local retval = mode_type
     if chromatic_steps ~= 12 then
         retval = retval .. " " .. chromatic_steps .. "-EDO"
     end
-    if mode_type == "Other" then
-        retval = retval .. "(" .. diatonic_steps .. " Steps)"
-    elseif key_mode:IsLinear() and (mode_type == "Linear" or tonal_center ~= 0) then
+    if diatonic_steps ~= 7 then
+        retval = retval .. " (" .. diatonic_steps .. " Steps)"
+    elseif key_mode:IsLinear() and (chromatic_steps == 12 or tonal_center ~= 0) then
         retval = retval .. " " .. linear_mode_types[(tonal_center % 7) + 1]
     elseif key_mode:IsNonLinear() then
         local notes = " " .. note_names[(tonal_center % 7) + 1] .. ":"
@@ -99,31 +93,137 @@ local function calc_key_mode_desc(key_mode)
             if not acci_order[x] then
                 break
             end
-            notes = notes .. " ".. note_names[(acci_order[x] % 7) + 1] .. tostring(alteration_names[acci_amounts[x]])
+            notes = notes .. " " .. note_names[(acci_order[x] % 7) + 1] .. tostring(alteration_names[acci_amounts[x]])
         end
         retval = retval .. notes
     end
     return retval
 end
 
+local function display_def(dialog, def)
+    assert(def:IsLinear() or def:IsNonLinear(), "key mode " .. def.ItemNo .. "is invalid")
+    local type_popup = dialog:GetControl("keymode_type")
+    type_popup:SetSelectedItem(def:IsLinear() and 0 or 1)
+    -- populate key map
+    local key_map = def.DiatonicStepsMap
+    key_map = key_map and #key_map > 0 and key_map or {0, 2, 4, 5, 7, 9, 11}
+    local num_steps = def.TotalChromaticSteps
+    num_steps = num_steps > 0 and num_steps or 12
+    for x = 1, math.min(#key_map, #note_names) do
+        local count = x < #key_map and key_map[x+1] - key_map[x] or num_steps - key_map[x]
+        dialog:GetControl("ds_" .. x):SetInteger(count)
+    end
+end
+
+local function select_keymode(dialog)
+    local popup = dialog:GetControl("keymodes")
+    local type_popup = dialog:GetControl("keymode_type")
+    local curr_selection = popup:GetSelectedItem()
+    if curr_selection == 0 then
+        display_def(dialog, finale.FCCustomKeyModeDef())
+        type_popup:SetEnable(true)
+    elseif curr_selection == 1 then
+        popup:SetSelection(context.current_selection)
+        return
+    else
+        local keymode = context.current_keymodes:GetItemAt(curr_selection - 2)
+        assert(keymode, "keymode not found for popup item " .. curr_selection)
+        display_def(dialog, keymode)
+        type_popup:SetEnable(false)
+    end
+    context.current_selection = curr_selection
+end
+
+local function on_document_change(dialog)
+    context.current_doc = finale.FCDocument().ID
+    local popup = dialog:GetControl("keymodes")
+        :Clear()
+        :AddString("< New >")
+    context.current_keymodes:LoadAll()
+    local x = 0
+    for def in each(context.current_keymodes) do
+        if x == 0 then
+            popup:AddString("-")
+        end
+        popup:AddString(calc_key_mode_desc(def))
+        x = x + 1
+    end
+    select_keymode(dialog)
+end
+
+local function on_popup(control)
+    local curr_selection = control:GetSelectedItem()
+    if curr_selection ~= context.current_selection then
+        select_keymode(control:GetParent())
+    end
+end
+
+local function on_timer(dialog, timer)
+    assert(timer == context.global_timer_id, "timer " .. timer .. " is not for this window")
+    local current_doc = finale.FCDocument().ID
+    if current_doc ~= context.current_doc then
+        on_document_change(dialog)
+    end
+end
+
+local function on_init_window(dialog)
+    on_timer(dialog, context.global_timer_id)
+    global_dialog:SetTimer(context.global_timer_id, 100) -- last step
+end
+
+local function on_close_window(_dialog)
+    global_dialog:StopTimer(context.global_timer_id) -- first step
+end
+
 local function create_dialog_box()
+    local padding = 5
+    local y_increment = 30
+    local curr_y = 0
+    local win_edit_offset = 5
+    local mac_edit_offset = 3
     local dlg = mixin.FCXCustomLuaWindow()
         :SetTitle(plugindef():gsub("%.%.%.", ""))
-    local popup = dlg:CreatePopup(0, 0, "keymodes")
+    -- keymode list
+    dlg:CreatePopup(0, curr_y, "keymodes")
+        :SetWidth(300)
+        :AddHandleCommand(on_popup)
+    dlg:CreatePopup(0, curr_y, "keymode_type")
+        :AddStrings("Linear", "Nonlinear")
         :DoAutoResizeWidth()
-    local defs = finale.FCCustomKeyModeDefs()
-    defs:LoadAll()
-    for def in each(defs) do
-        popup:AddString(calc_key_mode_desc(def))
+        :AssureNoHorizontalOverlap(dlg:GetControl("keymodes"), padding)
+    curr_y = curr_y + y_increment
+    -- diatonic steps
+    dlg:CreateStatic(0, curr_y, "diatonic_step_map")
+        :SetText("Diatonic Step Map")
+        :DoAutoResizeWidth()
+    for k, v in ipairs(note_names) do
+        local previous_control_name = k > 1 and "ds_" .. k - 1 or "diatonic_step_map"
+        local static = dlg:CreateStatic(0, curr_y)
+            :SetText(v)
+            :DoAutoResizeWidth(0)
+            :AssureNoHorizontalOverlap(dlg:GetControl(previous_control_name), padding)
+        dlg:CreateEdit(0, curr_y - utils.win_mac(win_edit_offset, mac_edit_offset), "ds_" .. k)
+            :SetWidth(25)
+            :AssureNoHorizontalOverlap(static, padding)
     end
-    dlg:CreateOkButton()
-    dlg:CreateCancelButton()
+    dlg:CreateStatic(0, curr_y)
+        :SetText(note_names[1])
+        :DoAutoResizeWidth()
+        :AssureNoHorizontalOverlap(dlg:GetControl("ds_" .. #note_names), padding)
+    curr_y = curr_y + y_increment
+    -- close button
+    dlg:CreateCloseButton(0, curr_y)
+        :HorizontallyAlignRightWithFurthest()
+    -- Registrations
+    dlg:RegisterInitWindow(on_init_window)
+    dlg:RegisterCloseWindow(on_close_window)
+    dlg:RegisterHandleTimer(on_timer)
     return dlg
 end
 
 local function key_modes_manage()
     global_dialog = global_dialog or create_dialog_box()
-    global_dialog:ExecuteModal()
+    global_dialog:RunModeless()
 end
 
 key_modes_manage()
