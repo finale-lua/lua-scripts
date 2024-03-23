fb0b89c8d8b531f3bef7726439f9bc59a3a31899
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2024-03-24 10:15:16 +1100

    Update pitch_changer.lua
    The last version of this script failed to properly recognise accidentals in all key signatures. The pitch-matching algorithm has been corrected along with other sundry improvements.

--- src/pitch_changer.lua
+++ src/pitch_changer.lua
@@ -4,8 +4,8 @@ function plugindef()
     finaleplugin.Author = "Carl Vine"
     finaleplugin.AuthorURL = "https://carlvine.com/lua/"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
-    finaleplugin.Version = "v0.29"
-    finaleplugin.Date = "2024/03/12"
+    finaleplugin.Version = "v0.32"
+    finaleplugin.Date = "2024/03/23"
     finaleplugin.AdditionalMenuOptions = [[
         Pitch Changer Repeat
     ]]
@@ -22,8 +22,8 @@ function plugindef()
     finaleplugin.ScriptGroupName = "Pitch Changer"
     finaleplugin.ScriptGroupDescription = "Change all notes of one pitch in the region to another pitch"
     finaleplugin.Notes = [[
-        This script was inspired by Jari Williamsson's _JW Change Pitches_ 
-        plug-in (2017) revived to work on Macs with non-Intel processors.
+        This script revives Jari Williamsson's _JW Change Pitches_ 
+        2017 plug-in to work on Macs with non-Intel processors.
 
         Identify __from__ and __to__ pitches by note name (__a-g__ or __A-G__) 
         followed by accidental (#-##-###, b-bb-bbb) as required. 
@@ -35,22 +35,22 @@ function plugindef()
 
         Select __Modeless__ if you prefer the dialog window to 
         "float" above your score so you can change the score selection 
-        while it's active. In this mode, click __Apply__ [Return/Enter] 
+        while it remains active. In this mode, click __Apply__ [Return/Enter] 
         to make changes and __Cancel__ [Escape] to close the window. 
         Cancelling __Modeless__ will apply the _next_ time you use the script.
 
         > __Key Commands:__ 
- 
+
         > - __a-g__ (__A-G__): Note Names
+        > - __0-4__: Layer number (delete key not needed)
         > - __z__: Direction Closest 
         > - __x__: Direction Up 
-        > - __v__: Direction Down 
-        > - __q__: Display these script notes 
+        > - __v__: Direction Down  
         > - __w__: Swap the __From:__ and __To:__ values 
         > - __s__: Shortcut for __#__ 
         > - __m__: Toggle the __Modeless__ setting 
         > - __r__: Toggle the __Written Pitch__ setting 
-        > - __0-4__: Layer number (delete key not needed) 
+        > - __q__: Display these script notes 
 	]]
     return "Pitch Changer...", "Pitch Changer", "Change all notes of one pitch in the region to another pitch"
 end
@@ -58,13 +58,13 @@ end
 repeat_change = repeat_change or false
 
 local config = {
-    find_string = "F#",
-    find_pitch = "F",
-    find_offset = 1, -- raise/lower value (to find)
-    new_string = "eb",
-    new_pitch = "E",
-    new_offset = -1, -- raise/lower value (to replace)
-    direction = 1, -- one-based index of "directions" choice
+    find_string = "F#", -- find this note
+    find_pitch = "F", -- its pitch name
+    find_offset = 1, -- its raise/lower value
+    new_string = "Eb", -- replace with this note
+    new_pitch = "E", -- its pitch name
+    new_offset = -1, -- its raise/lower value
+    direction = 1, -- one-based index of "direction" name [Closest/Up/Down]
     layer_num = 0,
     written_pitch = false,
     timer_id    = 1,
@@ -72,7 +72,7 @@ local config = {
     window_pos_x = false,
     window_pos_y = false,
 }
-local directions = { "Closest (z)", "Up (x)", "Down (v)" } -- 1 / 2 / 3
+
 local configuration = require("library.configuration")
 local layer = require("library.layer")
 local mixin = require("library.mixin")
@@ -86,6 +86,7 @@ local bounds = { -- primary region selection boundaries
     "StartStaff", "StartMeasure", "StartMeasurePos",
     "EndStaff",   "EndMeasure",   "EndMeasurePos",
 }
+local directions = { "Closest (z)", "Up (x)", "Down (v)" } -- 1 / 2 / 3
 
 local function dialog_set_position(dialog)
     if config.window_pos_x and config.window_pos_y then
@@ -117,7 +118,7 @@ local function get_staff_name(staff_num)
     return str
 end
 
-local function initialise_parameters()
+local function track_selection()
     -- set_saved_bounds
     local rgn = finenv.Region()
     for _, property in ipairs(bounds) do
@@ -139,62 +140,65 @@ local function initialise_parameters()
     end
 end
 
-local function calc_pitch_string(note)
-    local pitch_string = finale.FCString()
-    local cell = finale.FCCell(note.Entry.Measure, note.Entry.Staff)
-    local key_signature = cell:GetKeySignature()
-    note:GetString(pitch_string, key_signature, false, config.written_pitch)
-    return pitch_string.LuaString
-end
-
 local function decode_note_string(str)
-    local s = str:upper()
-    local pitch = s:sub(1, 1)
-    if s == "" or pitch:find("[^A-G]") then
-        return "", 0, 0
+    local pitch = str:upper():sub(1, 1)
+    local acci = str:sub(2):lower():gsub("s", "#")
+    if str == "" or pitch:find("[^A-G]") or
+        (acci:find("[^b#]") or (acci:find("b") and acci:find("#"))) then
+            return "", 0
     end
-    local raise_lower = 0
-    local acci = s:sub(2):gsub("S", "#")
-    if acci:find("[B#S]") then
-        for _ in acci:gmatch("B") do raise_lower = raise_lower - 1 end
-        for _ in acci:gmatch("#") do raise_lower = raise_lower + 1 end
-    end
+    local raise_lower = 0 -- count flats and sharps
+    for _ in acci:gmatch("b") do raise_lower = raise_lower - 1 end
+    for _ in acci:gmatch("#") do raise_lower = raise_lower + 1 end
     return pitch, raise_lower
 end
 
-local function displacement_direction(disp)
-    local direction = config.direction
-    if direction == 1 then -- "Closest"
-        if disp < -3 then disp = disp + 7
-        elseif disp > 3 then disp = disp - 7
+local function octave_direction()
+    local find = string.byte(config.find_pitch) - 67 -- "A" = -2, "C" = 0
+    local new = string.byte(config.new_pitch) - 67
+    local find_off, new_off = config.find_offset, config.new_offset
+    local oct = 0 -- octave change
+
+    if config.direction == 1 then -- "Closest"
+        if ((find - new) > 3 and (new_off < 0)) then
+            oct = 1
+        elseif ((new - find) > 3 and (find_off < 0)) then
+            oct = -1
         end
-    elseif direction == 2 then -- "Up"
-        if disp < 0 or (disp == 0 and config.new_offset < config.find_offset) then
-            disp = disp + 7
+    elseif config.direction == 2 then -- "Up"
+        if  (find < 0 and new > 0) or -- octave jumps around C
+            (new < find and (find < 0 or new >= 0)) or
+            (new == find and new_off < find_off) then
+            oct = 1 -- shift up octave
         end
-    elseif direction == 3 then -- "Down"
-        if disp > 0 or (disp == 0 and config.new_offset > config.find_offset) then
-            disp = disp - 7
+    else -- config.direction == 3 -- "Down"
+        if  (new < 0 and find > 0) or
+            (new > find and (find >= 0 or new < 0)) or
+            (new == find and new_off > find_off) then
+            oct = -1 -- shift down octave
         end
     end
-    return disp
+    return oct
 end
 
 local function change_the_pitches()
-    finenv.StartNewUndoBlock(string.format("Pitch Change %s to %s %s",
-        config.find_string, config.new_string, selection.region)
+    finenv.StartNewUndoBlock(
+        string.format("Pitch Change %s to %s %s",
+            config.find_string, config.new_string, selection.region)
     )
-    local displacement = string.byte(config.new_pitch) - string.byte(config.find_pitch)
-    displacement = displacement_direction(displacement) -- correct "direction" preference
+    local octave_change = octave_direction() -- get "direction" octave choice
+    local s = finale.FCString()
 
     for entry in eachentrysaved(finenv.Region(), config.layer_num) do
+        local key_sig = finale.FCCell(entry.Measure, entry.Staff):GetKeySignature()
         if entry:IsNote() then
             for note in each(entry) do
-                local pitch_string = calc_pitch_string(note)
-                local pitch, raise_lower = decode_note_string(pitch_string)
-                if pitch == config.find_pitch and raise_lower == config.find_offset then
-                    note.Displacement = note.Displacement + displacement
-                    note.RaiseLower = config.new_offset
+                note:GetString(s, key_sig, false, config.written_pitch)
+                local pitch_string = s.LuaString
+                local octave = pitch_string:sub(-1)
+                if config.find_string == pitch_string:sub(1, -2) then
+                    s.LuaString = config.new_string .. (octave + octave_change)
+                    note:SetString(s, key_sig, config.written_pitch)
                 end
             end
         end
@@ -209,28 +213,18 @@ local function run_the_dialog()
     local m_offset = finenv.UI():IsOnMac() and 3 or 0
     local y = 0
     local name = finaleplugin.ScriptGroupName
-    local pitch, errors = {}, {}
+    local pitch, ctl, errors = {}, {}, {}
     local save_text = { find = config.find_string, new = config.new_string }
 
     local dialog = mixin.FCXCustomLuaWindow():SetTitle(name)
         -- local functions
         local function yd(diff) y = y + (diff or 25) end
-        local function pitch_error_alert()
-            dialog:CreateChildUI():AlertError(
-                "Pitch names cannot be empty and must start with a single "
-                .. "note name (a-g or A-G) followed by accidentals "
-                .. "(#-###, b-bbb) as required.\n\n"
-                .. "These pitch names are invalid:\n"
-                .. table.concat(errors, "; "),
-                name .. " Error"
-            )
-        end
         local function show_info()
-            utils.show_notes_dialog(dialog, "About " .. name, 420, 415)
+            utils.show_notes_dialog(dialog, "About " .. name, 420, 430)
             refocus_document = true
         end
-        local function cstat(horiz, vert, wide, str, id)
-            dialog:CreateStatic(horiz, vert, id):SetWidth(wide):SetText(str)
+        local function cstat(horiz, vert, wide, str) -- dialog static text
+            return dialog:CreateStatic(horiz, vert):SetWidth(wide):SetText(str)
         end
         local function value_swap()
             pitch.new:SetText(save_text.find)
@@ -239,12 +233,10 @@ local function run_the_dialog()
             save_text.new = pitch.new:GetText()
         end
         local function toggle_check(id)
-            local m = dialog:GetControl(id)
-            m:SetCheck((m:GetCheck() + 1) % 2)
+            ctl[id]:SetCheck((ctl[id]:GetCheck() + 1) % 2)
         end
         local function key_substitutions(kind)
-            local t = pitch[kind]:GetText()
-            local s = t:upper()
+            local s = pitch[kind]:GetText():upper()
             if (kind == "layer" and s:find("[^0-4]"))
               or (kind ~= "layer" and s:find("[^A-G#]")) then
                 -- key substitutions:
@@ -261,8 +253,8 @@ local function run_the_dialog()
                 if kind == "layer" or not s:find("W") then
                     pitch[kind]:SetText(save_text[kind])
                 end
-            elseif s ~= "" then
-                s = (kind == "layer") and s:sub(-1) or (s:sub(1, 1) .. t:sub(2))
+            else
+                s = (kind == "layer") and s:sub(-1) or (s:sub(1, 1) .. s:sub(2):lower())
                 save_text[kind] = s
                 pitch[kind]:SetText(s)
             end
@@ -270,30 +262,30 @@ local function run_the_dialog()
         local function encode_pitches(kind)
             local s = pitch[kind]:GetText()
             local note, raise_lower = decode_note_string(s)
-            if note == "" or s:sub(2):upper():find("[^B#S]") then -- pitch name error
+            if note == "" then -- pitch name error
                 table.insert(errors, s) -- add error to list
                 return false -- flag user input error
             end
             config[kind .. "_pitch"] = note
             config[kind .. "_offset"] = raise_lower
             config[kind .. "_string"] = s
-            pitch[kind]:SetText(note .. s:sub(2))
-            save_text[kind] = note .. s:sub(2)
-            return true
+            pitch[kind]:SetText(s)
+            save_text[kind] = s
+            return true -- no errors
         end
-        local function on_timer() -- look for changes in selected region
+        local function on_timer() -- track changes in selected region
             for k, v in pairs(saved_bounds) do
                 if finenv.Region()[k] ~= v then -- selection changed
-                    initialise_parameters() -- update selection tracker
-                    dialog:GetControl("info1"):SetText(selection.staff)
-                    dialog:GetControl("info2"):SetText(selection.region)
+                    track_selection() -- update selection tracker
+                    ctl.info1:SetText(selection.staff)
+                    ctl.info2:SetText(selection.region)
                     break -- all done
                 end
             end
         end
-    cstat(x_pos[1], y, 50, "From:", "from")
-    cstat(x_pos[3], y, 50, "To:", "to")
-    cstat(x_pos[4], y, 60, "Direction:", "direction")
+    ctl.from = cstat(x_pos[1], y, 50, "From:")
+    ctl.to = cstat(x_pos[3], y, 50, "To:")
+    ctl.direction = cstat(x_pos[4], y, 60, "Direction:")
     yd(20)
     pitch.find = dialog:CreateEdit(x_pos[1], y - m_offset):SetWidth(40):SetText(config.find_string)
         :AddHandleCommand(function() key_substitutions("find") end)
@@ -311,19 +303,19 @@ local function run_the_dialog()
     save_text.layer = tostring(config.layer_num)
     pitch.layer = dialog:CreateEdit(60, y - m_offset):SetWidth(20):SetText(config.layer_num)
         :AddHandleCommand(function() key_substitutions("layer") end)
-    dialog:CreateCheckbox(x_pos[3] + 12, y, "written_pitch"):SetWidth(85)
+    ctl.written_pitch = dialog:CreateCheckbox(x_pos[3] + 12, y):SetWidth(85)
         :SetCheck(config.written_pitch and 1 or 0):SetText("Written Pitch")
-    dialog:CreateButton(x_pos[4] + 60, y, "q"):SetText("?"):SetWidth(20)
+    ctl.q = dialog:CreateButton(x_pos[4] + 60, y):SetText("?"):SetWidth(20)
        :AddHandleCommand(function() show_info() end)
     yd()
-    dialog:CreateCheckbox(0, y, "modeless"):SetWidth(x_pos[4] + 80)
+    ctl.modeless = dialog:CreateCheckbox(0, y):SetWidth(x_pos[4] + 80)
         :SetCheck(config.modeless and 1 or 0):SetText("\"Modeless\" Dialog")
     -- modeless selection info
     if config.modeless then
         yd(14)
-        dialog:CreateStatic(16, y, "info1"):SetText(selection.staff):SetWidth(x_pos[4] + 65)
+        ctl.info1 = dialog:CreateStatic(16, y):SetText(selection.staff):SetWidth(x_pos[4] + 65)
         yd(14)
-        dialog:CreateStatic(16, y, "info2"):SetText(selection.region):SetWidth(x_pos[4] + 65)
+        ctl.info2 = dialog:CreateStatic(16, y):SetText(selection.region):SetWidth(x_pos[4] + 65)
     end
     -- wrap it up
     dialog:CreateOkButton():SetText(config.modeless and "Apply" or "Change")
@@ -331,33 +323,38 @@ local function run_the_dialog()
     dialog:RegisterInitWindow(function(self)
         self:SetOkButtonCanClose(not config.modeless)
         if config.modeless then self:SetTimer(config.timer_id, 125) end
-        local f = self:GetControl("from")
-        local bold = f:CreateFontInfo():SetBold(true)
-        f:SetFont(bold)
-        self:GetControl("to"):SetFont(bold)
-        self:GetControl("direction"):SetFont(bold)
+        local bold = ctl.from:CreateFontInfo():SetBold(true)
+        ctl.from:SetFont(bold)
+        ctl.to:SetFont(bold)
+        ctl.direction:SetFont(bold)
         pitch.find:SetKeyboardFocus()
     end)
     local change_mode, user_error = false, false
     dialog_set_position(dialog)
     if config.modeless then dialog:RegisterHandleTimer(on_timer) end
-    dialog:RegisterHandleOkButtonPressed(function(self)
+    dialog:RegisterHandleOkButtonPressed(function()
         errors = {} -- empty error list
         local good_name1, good_name2 = encode_pitches("find"), encode_pitches("new")
         if good_name1 and good_name2 then -- no pitch name errors
             config.layer_num = pitch.layer:GetInteger()
             config.direction = pitch.popup:GetSelectedItem() + 1 -- one-based index
-            config.modeless = (self:GetControl("modeless"):GetCheck() == 1)
-            config.written_pitch = (self:GetControl("written_pitch"):GetCheck() == 1)
+            config.written_pitch = (ctl.written_pitch:GetCheck() == 1)
             change_the_pitches()
         else
-            pitch_error_alert()
+            dialog:CreateChildUI():AlertError( -- **ERROR!**
+                "Pitch names cannot be empty and must start with a single "
+                .. "note name (a-g or A-G) followed by accidentals "
+                .. "(#-###, b-bbb) as required.\n\n"
+                .. "These pitch names are invalid:\n"
+                .. table.concat(errors, "; "),
+                name .. " Error"
+            )
             user_error = true
         end
     end)
     dialog:RegisterCloseWindow(function(self)
         if config.modeless then self:StopTimer(config.timer_id) end
-        local mode = (self:GetControl("modeless"):GetCheck() == 1)
+        local mode = (ctl.modeless:GetCheck() == 1)
         change_mode = (mode and not config.modeless) -- modal -> modeless?
         config.modeless = mode
         dialog_save_position(self)
@@ -365,10 +362,10 @@ local function run_the_dialog()
     if config.modeless then   -- "modeless"
         dialog:RunModeless()
     else
-        dialog:ExecuteModal() -- "modal"
+        dialog:ExecuteModal()
         if refocus_document then finenv.UI():ActivateDocumentWindow() end
     end
-    return change_mode or user_error -- something still to change
+    return (change_mode or user_error) -- something still to change
 end
 
 local function change_pitch()
@@ -383,14 +380,11 @@ local function change_pitch()
     local qim = finenv.QueryInvokedModifierKeys
     local mod_key = qim and (qim(finale.CMDMODKEY_ALT) or qim(finale.CMDMODKEY_SHIFT))
 
-    initialise_parameters()
+    track_selection() -- track current selected region
     if mod_key or repeat_change then
         change_the_pitches()
     else
-        local unfinished = true
-        while unfinished do
-            unfinished = run_the_dialog()
-        end
+        while run_the_dialog() do end
     end
 end
 
