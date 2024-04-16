d21ebd0221081647cf75f8af64d1ac28b24f3465
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2024-04-16 10:29:39 +1000

    Update layer_actions.lua
    Amended layer “Erase” action to respect selection boundaries; upgraded Notes for `utils.show_notes_dialog`; script_name from general_library`; sundry minor improvements

--- src/layer_actions.lua
+++ src/layer_actions.lua
@@ -3,33 +3,28 @@ function plugindef()
     finaleplugin.Author = "Carl Vine"
     finaleplugin.AuthorURL = "https://carlvine.com/lua/"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
-    finaleplugin.Version = "v0.06"
-    finaleplugin.Date = "2023/10/15"
-    finaleplugin.MinJWLuaVersion = 0.62
+    finaleplugin.Version = "0.10"
+    finaleplugin.Date = "2024/04/16"
+    finaleplugin.MinJWLuaVersion = 0.70
     finaleplugin.Notes = [[
         Perform specific actions on individual note layers in the current selection. 
-        Each action in the list begins with a configurable hotkey. 
-        Open the script, type the hotkey and hit RETURN or ENTER. 
-        To repeat the same action as last time without a confirmation dialog 
-        hold down the SHIFT key when starting the script.
+        Each action in the list begins with a configurable _hotkey_. 
+        Open the script, type the _hotkey_ and hit [Return] or [Enter]. 
+        To repeat the same action as last time without a confirmation 
+        dialog hold down [Shift] when starting the script. 
+        Eight layer actions are available: 
 
-        Actions:
-        - Erase Layer
-        - Playback Enable
-        - Playback Mute
-        - Visible
-        - Invisible
-        - Stems Up
-        - Stems Down
-        - Stems Default
-
-        Note that "Erase Layer" will delete a whole measure even if only part of 
-        it is selected. All other actions respect selection boundaries. 
-        This script replaces four old (deprecated) ones in the repo: 
-        "layer_hide.lua", "layer_mute.lua", 
-        "stem_direction_by_layer.lua" and "layer_clear_selective.lua".
+        > - Erase Layer 
+        > - Playback Enable 
+        > - Playback Mute 
+        > - Visible 
+        > - Invisible 
+        > - Stems Up 
+        > - Stems Down 
+        > - Stems Default 
 	]]
-    return "Layer Actions...", "Layer Actions", "Perform specific actions on individual layers in the current selection"
+    return "Layer Actions...", "Layer Actions",
+        "Perform specific actions on individual layers in the current selection"
 end
 
 local dialog_options = { -- key, text description (ordered)
@@ -52,8 +47,8 @@ local config = { -- keystroke assignments and window position
     stems_up = "Q",
     stems_down = "W",
     stems_default = "E",
-    layer_num = "1",
-    last_selected = 0, -- last selected menu item number (0-based)
+    layer_num = 0,
+    selected = 0, -- last selected menu item number (0-based)
     window_pos_x = false,
     window_pos_y = false,
 }
@@ -61,7 +56,11 @@ local config = { -- keystroke assignments and window position
 local configuration = require("library.configuration")
 local mixin = require("library.mixin")
 local layer = require("library.layer")
-local script_name = "layer_actions"
+local utils = require("library.utils")
+local note_entry = require("library.note_entry")
+local library = require("library.general_library")
+local script_name = library.calc_script_name()
+local refocus_document = false -- set to true if utils.show_notes_dialog is used
 
 function dialog_set_position(dialog)
     if config.window_pos_x and config.window_pos_y then
@@ -81,25 +80,27 @@ end
 local function change_layer_state(state)
     local rgn = finenv.Region()
     local layer_num = tonumber(config.layer_num)
-    if state == "erase" then
-        if layer_num == 0 then
-            rgn:CutMusic()
-            rgn:ReleaseMusic()
-        else
-            layer.clear(rgn, layer_num)
-        end
-    else
+    if state == "erase" then -- LAYER ERASURE
         for entry in eachentrysaved(rgn, layer_num) do
-            local note = entry:IsNote()
-            if state:find("see") then
+            if entry:IsNote() then note_entry.make_rest(entry) end
+        end
+        for m, s in eachcell(rgn) do
+            local c = finale.FCNoteEntryCell(m, s)
+            c:Load()
+            c:ReduceEntries()
+            c:Save()
+        end
+    else -- other states are note-by-note
+        for entry in eachentrysaved(rgn, layer_num) do
+            if state:find("see") then -- notes and rests
                 entry.Visible = (state == "see_yes")
-            elseif state:find("play") then
-                if note then entry.Playback = (state == "play_yes") end
-            else -- if state:find("stems") then
-                if note then
-                    local default = (state == "stems_default")
-                    entry.FreezeStem = not default
-                    if not default then
+            elseif entry:IsNote() then -- notes only
+                if state:find("play") then
+                    entry.Playback = (state == "play_yes")
+                else -- stem condition
+                    local not_default = (state ~= "stems_default")
+                    entry.FreezeStem = not_default
+                    if not_default then
                         entry.StemUp = (state == "stems_up")
                     end
                 end
@@ -108,18 +109,18 @@ local function change_layer_state(state)
     end
 end
 
-local function reassign_keystrokes(index)
+local function reassign_keystrokes(parent, index)
     local y_step, x_wide = 17, 180
     local offset = finenv.UI():IsOnMac() and 3 or 0
     local is_duplicate, errors = false, {}
     local y = 0
-    local dialog = mixin.FCXCustomLuaWindow():SetTitle("Reassign Keys")
+    local dialog = mixin.FCXCustomLuaWindow():SetTitle("Layers: Reassign Keys")
 
     for _, v in ipairs(dialog_options) do -- add all options with keycodes
         dialog:CreateEdit(0, y - offset, v[1]):SetText(config[v[1]]):SetWidth(20)
             :AddHandleCommand(function(self)
-                local str = self:GetText():upper()
-                self:SetText(str:sub(-1)):SetKeyboardFocus()
+                local str = self:GetText():sub(-1):upper()
+                self:SetText(str):SetKeyboardFocus()
             end)
         dialog:CreateStatic(25, y):SetText(v[2]):SetWidth(x_wide)
         y = y + y_step
@@ -136,7 +137,7 @@ local function reassign_keystrokes(index)
     dialog:RegisterHandleOkButtonPressed(function(self)
         local assigned = {}
         for i, v in ipairs(dialog_options) do
-            local key = self:GetControl(v[1]):GetText() or "?"
+            local key = self:GetControl(v[1]):GetText()
             if key == "" then key = "?" end -- not null
             config[v[1]] = key -- save for another possible run-through
             config.ignore_duplicates = ignore:GetCheck()
@@ -163,39 +164,41 @@ local function reassign_keystrokes(index)
             finenv.UI():AlertError(msg, "Duplicate Key Assignment")
         end
     end)
-    local ok = (dialog:ExecuteModal(nil) == finale.EXECMODAL_OK)
+    local ok = (dialog:ExecuteModal(parent) == finale.EXECMODAL_OK)
     return ok, is_duplicate
 end
 
 local function user_chooses()
     local max = layer.max_layers()
-    local offset = finenv.UI():IsOnMac() and 3 or 0
     local y, y_step = 0, 17
     local box_wide = 220
     local box_high = (#dialog_options * y_step) + 4
+    local name = plugindef():gsub("%.%.%.", "")
     local start, stop = finenv.Region().StartMeasure, finenv.Region().EndMeasure
     local message = (start == stop) and ("(m." .. start) or ("(mm." .. start .. "-" .. stop)
-    local notes = finaleplugin.Notes:gsub(" %s+", " "):gsub("\n ", "\n"):sub(2)
-    local function show_info() finenv.UI():AlertInfo(notes, "About " .. plugindef()) end
 
-    local dialog = mixin.FCXCustomLuaWindow():SetTitle(plugindef())
+    local dialog = mixin.FCXCustomLuaWindow():SetTitle(name)
     dialog:CreateStatic(0, y):SetText("Choose Layer Action:"):SetWidth(box_wide)
     dialog:CreateStatic(box_wide - 80, y):SetText(message .. ")"):SetWidth(80)
     y = y + 20
     local key_list = dialog:CreateListBox(0, y):SetWidth(box_wide):SetHeight(box_high)
-
+        -- local functions
+        local function show_info()
+            utils.show_notes_dialog(dialog, "About " .. name, 400, 290)
+            refocus_document = true
+        end
         local function fill_key_list()
             local join = finenv.UI():IsOnMac() and "\t" or ": "
             key_list:Clear()
             for _, option in ipairs(dialog_options) do -- add ordered options with keycodes
                 key_list:AddString(config[option[1]] .. join .. option[2])
             end
-            key_list:SetSelectedItem(config.last_selected)
+            key_list:SetSelectedItem(config.selected)
         end
         local function change_keys()
             local ok, is_duplicate = true, true
             while ok and is_duplicate do -- wait for valid choice in reassign_keystrokes()
-                ok, is_duplicate = reassign_keystrokes(key_list:GetSelectedItem() + 1)
+                ok, is_duplicate = reassign_keystrokes(dialog, key_list:GetSelectedItem() + 1)
             end
             if ok then fill_key_list() end
         end
@@ -204,22 +207,21 @@ local function user_chooses()
     local x_off = box_wide / 4
     y = y + box_high + 7
     dialog:CreateStatic(0, y):SetWidth(x_off + 4):SetText("Layer 1-" .. max .. ":")
-    local save_layer = config.layer_num
-    local layer_num = dialog:CreateEdit(x_off + 4, y - offset):SetWidth(30):SetText(save_layer)
+    local save_layer = tostring(config.layer_num)
+    local offset = finenv.UI():IsOnMac() and 3 or 0
+    local layer_num = dialog:CreateEdit(x_off + 4, y - offset):SetWidth(20):SetText(save_layer)
         :AddHandleCommand(function(self)
             local val = self:GetText():lower()
-            if val:find("[^0-4]") then
+            if val:find("[^0-" .. max .. "]") then
                 if val:find("r") then change_keys()
-                elseif val:find("q") then show_info()
+                elseif val:find("[?q]") then show_info()
                 end
-                self:SetText(save_layer):SetKeyboardFocus()
-            else
-                val = val:sub(-1)
-                self:SetText(val)
-                save_layer = val
+            elseif val ~= "" then
+                save_layer = val:sub(-1)
             end
+            self:SetText(save_layer):SetKeyboardFocus()
         end)
-    dialog:CreateStatic(x_off + 36, y):SetWidth(80):SetText("(0 = all)")
+    dialog:CreateStatic(x_off + 26, y):SetWidth(60):SetText("(0 = all)")
     y = y + y_step + 2
     dialog:CreateButton(0, y)
         :SetText("Reassign Hotkeys"):SetWidth(x_off * 2) -- half box width
@@ -233,20 +235,21 @@ local function user_chooses()
     dialog:RegisterCloseWindow(function(self) dialog_save_position(self) end)
     dialog:RegisterHandleOkButtonPressed(function()
         config.layer_num = layer_num:GetInteger()
-        config.last_selected = key_list:GetSelectedItem() -- list choice, 0-based
+        config.selected = key_list:GetSelectedItem() -- list choice, 0-based
     end)
     return (dialog:ExecuteModal(nil) == finale.EXECMODAL_OK)
 end
 
 local function layer_state()
     configuration.get_user_settings(script_name, config, true)
-    local shift_key = finenv.QueryInvokedModifierKeys and finenv.QueryInvokedModifierKeys(finale.CMDMODKEY_SHIFT)
+    local qim = finenv.QueryInvokedModifierKeys
+    local mod_key = qim and (qim(finale.CMDMODKEY_ALT) or qim(finale.CMDMODKEY_SHIFT))
 
-    if shift_key or user_chooses() then
-        local choice = config.last_selected + 1
+    if mod_key or user_chooses() then
+        local choice = config.selected + 1
         change_layer_state(dialog_options[choice][1])
     end
-    finenv.UI():ActivateDocumentWindow()
+    if refocus_document then finenv.UI():ActivateDocumentWindow() end
 end
 
 layer_state()
