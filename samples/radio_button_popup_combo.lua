88517482c11bb8f6881d0bba82c38a4c3f5011e2
Robert Patterson <robert@robertgpatterson.com>
2024-04-19 09:34:56 -0500

    win refinements

--- samples/radio_button_popup_combo.lua
+++ samples/radio_button_popup_combo.lua
@@ -13,6 +13,8 @@ function plugindef()
     return "0-radio_button_popup_combo.lua"
 end
 
+local utils = require("library.utils")
+
 local dialog = finale.FCCustomLuaWindow()
 dialog:SetTitle(finale.FCString("Radio Group Test"))
 local static = dialog:CreateStatic(0, 0)
@@ -20,7 +22,7 @@ static:SetText(finale.FCString("Select Radio Button"))
 static:SetWidth(150)
 local my_list = { "Here", "are", "some", "radio", "button", "texts" }
 local radio_top = 20
-local popup_spacing = 23
+local popup_spacing = utils.win_mac(21, 23)
 local radio = dialog:CreateRadioButtonGroup(0, radio_top, #my_list) -- topmost radion button is at radio_top
 local popups = {}
 for index, value in ipairs(my_list) do
@@ -33,7 +35,7 @@ for index, value in ipairs(my_list) do
         popup:AddString(finale.FCString(value .. " option " .. i))
     end
     popup:AssureNoHorizontalOverlap(rad, 5) -- popups are at least 5 pixels from the radio button text
-    popup:VerticallyAlignWith(rad, -1) -- popups are offet -1 from the top of the radio buttons
+    popup:VerticallyAlignWith(rad, utils.win_mac(-2,-1)) -- popups are offet -1 from the top of the radio buttons
     popup:DoAutoResizeWidth() -- popup buttons are set to have a width *at least* as wide as the text (omitting the parameter does this)
     if index > 1 then
         popup:HorizontallyAlignLeftWith(popups[1])
