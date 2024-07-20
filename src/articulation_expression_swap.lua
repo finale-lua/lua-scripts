2adc0088610f4a48d0136d684e10da7bcb049800
MuseCraft Studio <themusecrafter@gmail.com>
2024-07-20 18:12:04 -0400

    carl vine edits integrated

--- src/articulation_expression_swap.lua
+++ src/articulation_expression_swap.lua
@@ -2,20 +2,31 @@ function plugindef()
     finaleplugin.RequireSelection = false
     finaleplugin.Author = "CJ Garcia"
     finaleplugin.Copyright = "2024 MuseCraft Studio"
-    finaleplugin.Version = "1.0"
-    finaleplugin.Date = "5/27/2024"
+    finaleplugin.Version = "1.1"
+    finaleplugin.Date = "7/20/2024"
+    finaleplugin.MinJWLuaVersion = 0.66
     finaleplugin.RevisionNotes = [[
         May 27, 2024: Script work began with idea from Burt Goldstein
         May 28, 2024: Version 1.0
+        July 20, 2024: Carl Vine edits integrated
         ]]
     finaleplugin.CategoryTags = "Articulation, Expression"
-    return "Articulation and Expression Swap", "Articulation and Expression Swap",
+    return "Articulation and Expression Swap...", "Articulation and Expression Swap",
         "Replaces the selected articulation with the selected expression (or vice versa) for the full document if a region is not selected."
 end
 
-SelectedItem = nil
+local config = {
+    art_id = 0,
+    exp_id = 0,
+    show_result = true,
+}
+local configuration = require("library.configuration")
+local library = require("library.general_library")
+local script_name = library.calc_script_name()
+local name = plugindef():gsub("%.%.%.", "")
+configuration.get_user_settings(script_name, config, true)
 
-local function assignExpression(exp_id, entry)
+local function assign_expression(exp_id, entry)
     local exp = finale.FCExpression()
     exp:SetStaff(entry.Staff)
     exp:SetVisible(true)
@@ -23,69 +34,66 @@ local function assignExpression(exp_id, entry)
     exp:SetScaleWithEntry(true)
     exp:SetLayerAssignment(entry.LayerNumber)
     exp:SetID(exp_id)
-
-    local note_cell = finale.FCCell(entry.Measure, entry.Staff)
-    exp:SaveNewToCell(note_cell)
+    exp:SaveNewToCell(finale.FCCell(entry.Measure, entry.Staff))
 end
 
-local function run(art_id, exp_id)
-    if (art_id == nil) and (exp_id ~= nil) then
-        finenv.UI():AlertInfo("Articulation ID must be a number. Exiting process", "Articulation entry error")
-        return
-    elseif (art_id ~= nil) and (exp_id == nil) then
-        finenv.UI():AlertInfo("Expression ID must be a number. Exiting process", "Expression entry error")
-        return
-    elseif (art_id == nil) and (exp_id == nil) then
-        finenv.UI():AlertInfo("The articulation ID and expression ID must both be a number. Exiting process",
-            "Entry error")
-        return
-    end
-
-    local art_defs = finale.FCArticulationDefs()
-    art_defs:LoadAll()
-    local has_art = false
-    for art in each(art_defs) do
-        if art:GetItemNo() == art_id then
-            has_art = true
-            break
+local function show_the_results(type, count, id)
+    if config.show_result then
+        if count ~= 0 then
+            local msg = count > 1 and " occurances of " or " occurance of "
+            msg = msg .. type .. " ID " .. id
+            finenv.UI():AlertInfo("Replaced " .. count .. msg, name .. ": Success")
+        else
+            finenv.UI():AlertInfo("No occurances of " .. type .. " ID " .. id .. " were found",
+                name .. ": Not found")
         end
     end
+end
 
-    local exp_defs = finale.FCTextExpressionDefs()
-    exp_defs:LoadAll()
-    local has_exp = false
-    for ted in each(exp_defs) do
-        if ted:GetItemNo() == exp_id then
-            has_exp = true
-            break
-        end
+local function run(art_id, exp_id, selected_item)
+    local msg = {}
+    if art_id == nil or art_id < 1 then
+        table.insert(msg, "The Articulation ID must be a digit > 0")
+    end
+    if exp_id == nil or exp_id < 1 then
+        table.insert(msg, "The Expression ID must be a digit > 0")
+    end
+    if #msg > 0 then
+        table.insert(msg, "Exiting process.")
+        finenv.UI():AlertInfo(table.concat(msg, "\n\n"), name .. ": Entry Error")
+        return
     end
 
-    if (has_art ~= true) and (has_exp == true) then
-        finenv.UI():AlertInfo("The articulation with ID " .. art_id .. " could not be found. Exiting process",
-            "Unable to find articulation")
-        return
-    elseif (has_art == true) and (has_exp ~= true) then
-        finenv.UI():AlertInfo("The expression with ID " .. exp_id .. " could not be found. Exiting process",
-            "Unable to find expression")
-        return
-    elseif (has_art ~= true) and (has_exp ~= true) then
-        finenv.UI():AlertInfo(
-            "Neither the articulation with ID " ..
-            art_id .. " nor the expression with ID " .. exp_id .. " could not be found. Exiting process",
-            "Unable to find items")
+    local function match_item(fc_defs, id)
+        fc_defs:LoadAll()
+        for a in each(fc_defs) do
+            if a:GetItemNo() == id then return true end
+        end
+        return false
+    end
+    local has_art = match_item(finale.FCArticulationDefs(), art_id)
+    local has_exp = match_item(finale.FCTextExpressionDefs(), exp_id)
+    msg = {}
+    if not has_art then
+        table.insert(msg, "Articulation ID " .. art_id .. " could not be found.")
+    end
+    if not has_exp then
+        table.insert(msg, "Expression ID " .. exp_id .. " could not be found.")
+    end
+    if #msg > 0 then
+        table.insert(msg, "Exiting process.")
+        finenv.UI():AlertInfo(table.concat(msg, "\n\n"), name .. ": Items not found")
         return
     end
 
     local music_region = finenv.Region()
-    if music_region:IsEmpty() == true then
+    if music_region:IsEmpty() then
         music_region = finale.FCMusicRegion()
         music_region:SetFullDocument()
     end
 
     local count = 0
-
-    if SelectedItem == 0 then
+    if selected_item == 0 then
         -- replace articulation with expression
         for noteentry in eachentrysaved(music_region) do
             local arts = noteentry:CreateArticulations()
@@ -93,22 +101,11 @@ local function run(art_id, exp_id)
                 if a:GetID() == art_id then
                     count = count + 1
                     a:DeleteData()
-                    assignExpression(exp_id, noteentry)
+                    assign_expression(exp_id, noteentry)
                 end
             end
         end
-        if count ~= 0 then
-            if count > 1 then
-                finenv.UI():AlertInfo("Replaced " .. count .. " occurances of articulation with the ID of " .. art_id,
-                    "Success")
-            else
-                finenv.UI():AlertInfo("Replaced " .. count .. " occurance of articulation with the ID of " .. art_id,
-                    "Success")
-            end
-        else
-            finenv.UI():AlertInfo("No occurances of articulation with the ID of " .. art_id .. " was found.",
-                "Nothing found")
-        end
+        show_the_results("articulation", count, art_id)
     else
         -- replace expression with articulation
         for noteentry in eachentrysaved(music_region) do
@@ -134,74 +131,59 @@ local function run(art_id, exp_id)
                 end
             end
         end
-        if count ~= 0 then
-            if count > 1 then
-                finenv.UI():AlertInfo("Replaced " .. count .. " occurances of expression with the ID of " .. exp_id,
-                    "Success")
-            else
-                finenv.UI():AlertInfo("Replaced " .. count .. " occurance of expression with the ID of " .. exp_id,
-                    "Success")
-            end
-        else
-            finenv.UI():AlertInfo("No occurances of expression with the ID of " .. exp_id .. " was found.",
-                "Nothing found")
-        end
+        show_the_results("expression", count, exp_id)
     end
 end
 
-local str = finale.FCString()
-str.LuaString = "Articulation Replacement"
+local fs = finale.FCString
 local dialog = finale.FCCustomLuaWindow()
-dialog:SetTitle(str)
+dialog:SetTitle(fs(name))
 
 local art_button = dialog:CreateButton(0, 45)
-str.LuaString = "Select..."
-art_button:SetText(str)
-
+art_button:SetText(fs("Select..."))
 local exp_button = dialog:CreateButton(150, 45)
-str.LuaString = "Select..."
-exp_button:SetText(str)
+exp_button:SetText(fs("Select..."))
 
 local art_text = dialog:CreateStatic(0, 0)
-str.LuaString = "Articulation ID"
-art_text:SetText(str)
+art_text:SetText(fs("Articulation ID"))
 local art_box = dialog:CreateEdit(0, 16)
-local art_str = finale.FCString()
-art_str.LuaString = ""
+art_box:SetInteger(config.art_id)
 
 local exp_text = dialog:CreateStatic(150, 0)
-str.LuaString = "Expression ID"
-exp_text:SetText(str)
+exp_text:SetText(fs("Expression ID"))
 local exp_box = dialog:CreateEdit(150, 16)
-local exp_str = finale.FCString()
-exp_str.LuaString = ""
+exp_box:SetInteger(config.exp_id)
 
-local radio_group = dialog:CreateRadioButtonGroup(0, 100, 2)
+local radio_group = dialog:CreateRadioButtonGroup(0, 70, 2)
 local strs = finale.FCStrings()
-strs:AddCopy(finale.FCString("Find articulation, replace with expression."))
-strs:AddCopy(finale.FCString("Find expression, replace with articulation."))
+strs:CopyFromStringTable{
+    "Find Articulation, Replace With Expression",
+    "Find Expression, Replace With Articulation"
+}
 radio_group:SetText(strs)
-radio_group:SetWidth(225)
+radio_group:SetWidth(240)
+local show_result = dialog:CreateCheckbox(0, 105)
+show_result:SetWidth(150)
+show_result:SetText(fs("Show Results"))
+show_result:SetCheck(config.show_result and 1 or 0)
 
 dialog:CreateOkButton()
-
 dialog:CreateCancelButton()
 
-local function getUserSelection(controller)
+local function get_user_selection(controller)
     if controller:GetControlID() == art_button:GetControlID() then
-        local art_select = finenv.UI():DisplayArticulationDialog(0)
-        art_box:SetText(finale.FCString(tostring(art_select)))
+        art_box:SetInteger(finenv.UI():DisplayArticulationDialog(art_box:GetInteger()))
     elseif controller:GetControlID() == exp_button:GetControlID() then
-        local art_select = finenv.UI():DisplayExpressionDialog(0, false)
-        exp_box:SetText(finale.FCString(tostring(art_select)))
+        exp_box:SetInteger(finenv.UI():DisplayExpressionDialog(exp_box:GetInteger(), false))
     end
 end
 
-dialog:RegisterHandleCommand(getUserSelection)
+dialog:RegisterHandleCommand(get_user_selection)
 
 if dialog:ExecuteModal(nil) == finale.EXECMODAL_OK then
-    art_box:GetText(art_str)
-    exp_box:GetText(exp_str)
-    SelectedItem = radio_group:GetSelectedItem()
-    run(tonumber(art_str.LuaString), tonumber(exp_str.LuaString))
+    config.art_id = art_box:GetInteger()
+    config.exp_id = exp_box:GetInteger()
+    config.show_result = (show_result:GetCheck() == 1)
+    configuration.save_user_settings(script_name, config)
+    run(config.art_id, config.exp_id, radio_group:GetSelectedItem())
 end
