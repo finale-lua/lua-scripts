ec6caf16426e6f8452eded6a8ea8ef7608330eb0
cv-on-hub <105578039+cv-on-hub@users.noreply.github.com>
2024-07-26 14:46:09 +1000

    Update expression_playback_layer.lua
    Just discovered the "special" playback layer numbers 0, 5 and 6, now incorporated. Added Script Info button to explain them, and moved the test for nil selection to a better position.

--- src/expression_playback_layer.lua
+++ src/expression_playback_layer.lua
@@ -3,11 +3,14 @@ function plugindef()
     finaleplugin.Author = "Carl Vine"
     finaleplugin.AuthorURL = "http://carlvine.com/lua/"
     finaleplugin.Copyright = "https://creativecommons.org/licenses/by/4.0/"
-    finaleplugin.Version = "0.03"
-    finaleplugin.Date = "2024/07/25"
+    finaleplugin.Version = "0.04"
+    finaleplugin.Date = "2024/07/26"
     finaleplugin.Notes = [[
         Change the assigned playback layer for all expressions in the current selection. 
-        Layer "0" is interpreted as "Current Layer". 
+        Layers __1-4__ are the _basic_ playback layers. 
+        Layer numbers __0__, __5__ and __6__ represent, respectively, 
+        __Current__, __Chord__ and __Expression__ Layers for playback. 
+
         Hold down _Shift_ when starting the script to repeat the same action 
         as last time without a confirmation dialog.
     ]]
@@ -23,9 +26,10 @@ local c = { -- user config values
 }
 local configuration = require("library.configuration")
 local mixin = require("library.mixin")
+local utils = require("library.utils")
 local library = require("library.general_library")
-local layer = require("library.layer")
 local script_name = library.calc_script_name()
+local refocus_document = false -- set to true if utils.show_notes_dialog is used
 
 local function dialog_set_position(dialog)
     if c.window_pos_x and c.window_pos_y then
@@ -53,10 +57,15 @@ local function user_dialog()
     local layer_num = dialog:CreateEdit(55, y - y_off):SetInteger(save):SetWidth(20)
         :AddHandleCommand(function(self)
             local val = self:GetText():lower()
-            if not val:find("[^0-" .. layer.max_layers() .. "]") then
+            if not val:find("[^0-6]") then
                 save = tonumber(val:sub(-1)) or 0
             end
             self:SetInteger(save):SetKeyboardFocus()
+        end)
+    dialog:CreateButton(165, y, "q"):SetText("?"):SetWidth(20)
+        :AddHandleCommand(function()
+            utils.show_notes_dialog(dialog, plugindef():gsub("%.%.%.", ""), 300, 150)
+            refocus_document = true
         end)
     -- all ready
     dialog:CreateOkButton()
@@ -68,27 +77,27 @@ local function user_dialog()
 end
 
 local function playback_layer()
+    if finenv.Region():IsEmpty() then
+        finenv.UI():AlertError(
+            "Please select some music\nbefore running this script",
+            "Playback Layer")
+        return
+    end
     configuration.get_user_settings(script_name, c, true)
     local qim = finenv.QueryInvokedModifierKeys
     local mod_key = qim and (qim(finale.CMDMODKEY_ALT) or qim(finale.CMDMODKEY_SHIFT))
 
     if mod_key or user_dialog() then
-        if finenv.Region():IsEmpty() then
-            finenv.UI():AlertError(
-                "Please select some music\nbefore running this script",
-                "Playback Layer")
-            return
-        else
-            local expressions = finale.FCExpressions()
-            expressions:LoadAllForRegion(finenv.Region())
-            for exp in each(expressions) do
-                if exp.StaffGroupID == 0 then -- exclude "Staff List" expressions
-                    exp.PlaybackLayerAssignment = c.layer
-                    exp:Save()
-                end
+        local expressions = finale.FCExpressions()
+        expressions:LoadAllForRegion(finenv.Region())
+        for exp in each(expressions) do
+            if exp.StaffGroupID == 0 then -- exclude "Staff List" expressions
+                exp.PlaybackLayerAssignment = c.layer
+                exp:Save()
             end
         end
     end
+    if refocus_document then finenv.UI():ActivateDocumentWindow() end
 end
 
 playback_layer()
